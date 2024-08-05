7bb16e0bcc294efa7f00d1afaaa5b1facd3629f9
Jason Zimdars <jz@37signals.com>
2024-08-05 17:10:38 -0500

    Refine new splat shape

--- app/models/splat.rb
+++ app/models/splat.rb
@@ -2,5 +2,5 @@ class Splat < ApplicationRecord
   has_many :categorizations
   has_many :categories, through: :categorizations, dependent: :destroy
 
-  enum :color, %w[ dodgerblue limegreen tomato mediumorchid ].index_by(&:itself), suffix: true, default: :dodgerblue
+  enum :color, %w[ dodgerblue teal tomato slateblue ].index_by(&:itself), suffix: true, default: :dodgerblue
 end
