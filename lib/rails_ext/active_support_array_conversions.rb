41164012f4a5f256fcd7d89e2b46c783a8f34037
Jose Farias <jose@37signals.com>
2024-10-17 22:01:25 -0600

    Appease zeitwerk

--- lib/rails_ext/active_support_array_conversions.rb
+++ lib/rails_ext/active_support_array_conversions.rb
@@ -1,5 +1,9 @@
-class Array
-  def to_choice_sentence
-    to_sentence two_words_connector: " or ", last_word_connector: ", or "
+module RailsExt
+  module ActiveSupportArrayConversions
+    def to_choice_sentence
+      to_sentence two_words_connector: " or ", last_word_connector: ", or "
+    end
   end
 end
+
+Array.include RailsExt::ActiveSupportArrayConversions
