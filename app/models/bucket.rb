8609e767983c12e1c3e00066a078b9b6b6cc8458
Jose Farias <jose@37signals.com>
2024-10-17 22:51:02 -0600

    Move bubble filtering to the model

--- app/models/bucket.rb
+++ app/models/bucket.rb
@@ -1,5 +1,5 @@
 class Bucket < ApplicationRecord
-  include Accessible, Views
+  include Accessible, Filterable, Views
 
   belongs_to :account
   belongs_to :creator, class_name: "User", default: -> { Current.user }
