b7023dfb705ca4ed7c507233ebb390484da879d9
David Heinemeier Hansson <david@hey.com>
2024-10-19 19:55:14 -0700

    Already comes as a hash

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,7 @@ class CommentsController < ApplicationController
   include BubbleScoped, BucketScoped
 
   def create
-    @bubble.comments.create! body: params.expect(comment: [ :body ])
+    @bubble.comments.create! params.expect(comment: [ :body ])
     redirect_to @bubble
   end
 end
