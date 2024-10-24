416b1f560b13e745a31bfc8f9adc3a428e38319a
Jose Farias <jose@37signals.com>
2024-10-24 16:04:12 -0600

    Might be ok for some events not to have rollups
    e.g. if we add events that don't get rolled up

--- db/migrate/20241022180133_add_rollup_reference_to_events.rb
+++ db/migrate/20241022180133_add_rollup_reference_to_events.rb
@@ -1,6 +1,5 @@
 class AddRollupReferenceToEvents < ActiveRecord::Migration[8.0]
   def up
-    # FIXME: add null: false in another migration after data migrations happen
     add_reference :events, :rollup, foreign_key: true
   end
 end
