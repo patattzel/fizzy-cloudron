0f02b47b1c008abf277ef5058e8ae3a4ffcba53c
Jose Farias <jose@37signals.com>
2024-10-25 16:06:43 -0600

    Unnecessary `after_touch`

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,7 +3,5 @@ class Message < ApplicationRecord
 
   delegated_type :messageable, types: Messageable::TYPES, inverse_of: :message, dependent: :destroy
 
-  after_touch -> { bubble.touch }
-
   scope :chronologically, -> { order created_at: :asc, id: :desc }
 end
