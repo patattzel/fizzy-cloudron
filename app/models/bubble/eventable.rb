7e439e3a7b703d132a25a8ae3515e47dad376bb3
Jose Farias <jose@37signals.com>
2024-10-28 10:47:49 -0600

    create -> create!

--- app/models/bubble/eventable.rb
+++ app/models/bubble/eventable.rb
@@ -7,7 +7,7 @@ module Bubble::Eventable
 
   private
     def track_event(action, creator: Current.user, **particulars)
-      find_or_capture_event_summary.events.create action: action, creator: creator, particulars: particulars
+      find_or_capture_event_summary.events.create! action: action, creator: creator, particulars: particulars
     end
 
     def find_or_capture_event_summary
