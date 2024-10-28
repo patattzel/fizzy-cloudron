f4c9dfc33c69a64e68fa8e36665d27496d81d368
Jose Farias <jose@37signals.com>
2024-10-28 10:23:40 -0600

    Pull out `new_comment_placeholder` helper

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -3,4 +3,12 @@ module CommentsHelper
     tag.div id: dom_id(comment), class: "comment flex align-start full-width",
       data: { creator_id: comment.creator_id, created_by_current_user_target: "creation" }, &
   end
+
+  def new_comment_placeholder(bubble)
+    if bubble.creator == Current.user && bubble.messages.comments.empty?
+      "Add some notes…"
+    else
+      "Type your comment…"
+    end
+  end
 end
