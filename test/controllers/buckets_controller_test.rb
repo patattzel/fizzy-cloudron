6789b2d0974fb414c1950690d7898e31db7134b2
Jose Farias <jose@37signals.com>
2024-11-05 14:14:33 -0600

    Fix tests

--- test/controllers/buckets_controller_test.rb
+++ test/controllers/buckets_controller_test.rb
@@ -21,7 +21,7 @@ class BucketsControllerTest < ActionDispatch::IntegrationTest
     end
 
     bucket = Bucket.last
-    assert_redirected_to bucket_bubbles_url(bucket)
+    assert_redirected_to bubbles_path(bucket_ids: bucket.id)
     assert_includes bucket.users, users(:kevin)
     assert_equal "Remodel Punch List", bucket.name
   end
@@ -34,7 +34,7 @@ class BucketsControllerTest < ActionDispatch::IntegrationTest
   test "update" do
     patch bucket_url(buckets(:writebook)), params: { bucket: { name: "Writebook bugs" }, user_ids: users(:david, :jz).pluck(:id) }
 
-    assert_redirected_to bucket_bubbles_url(buckets(:writebook))
+    assert_redirected_to bubbles_path(bucket_ids: buckets(:writebook))
     assert_equal "Writebook bugs", buckets(:writebook).reload.name
     assert_equal users(:david, :jz), buckets(:writebook).users
   end
