2cf691f0ae300800b5c93bdf3f71f89f45a5f5ec
Jose Farias <jose@37signals.com>
2024-11-07 11:32:21 -0600

    Comment on the query merger controller

--- app/javascript/controllers/query_merger_controller.js
+++ app/javascript/controllers/query_merger_controller.js
@@ -1,5 +1,6 @@
 import { Controller } from "@hotwired/stimulus"
 
+// FIXME: Can we do this without a controller? https://github.com/basecamp/fizzy/pull/130#discussion_r1833094616
 export default class extends Controller {
   merge({ params: { key, value } }) {
     const url = new URL(window.location.href)
