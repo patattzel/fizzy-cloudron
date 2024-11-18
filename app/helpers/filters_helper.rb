4697d20702ecf1176577c9f4a2b20e467fff4a49
Jose Farias <jose@37signals.com>
2024-11-18 11:45:29 -0600

    Pass params directly into filter chip helper

--- app/helpers/filters_helper.rb
+++ app/helpers/filters_helper.rb
@@ -1,6 +1,6 @@
 module FiltersHelper
-  def filter_chip_tag(text, name:, value:, filter:)
-    link_to bubbles_path(**filter.params_without(name, value)), class: "btn txt-small btn--remove" do
+  def filter_chip_tag(text, **)
+    link_to bubbles_path(**), class: "btn txt-small btn--remove" do
       concat tag.span(text)
       concat image_tag("close.svg", aria: { hidden: true }, size: 24)
     end
