24468c00275cb83fd92d61d1ed20e0780d773c3f
Jason Zimdars <jz@37signals.com>
2024-11-19 12:45:16 -0600

    Add some structure to buckets index

--- app/models/filter/summarized.rb
+++ app/models/filter/summarized.rb
@@ -9,18 +9,18 @@ module Filter::Summarized
 
   private
     def index_summary
-      "<mark>#{indexed_by.humanize}</mark>"
+      indexed_by.humanize
     end
 
     def tag_summary
       if tags.any?
-        "tagged <mark>#{tags.map(&:hashtag).to_choice_sentence}</mark>"
+        "tagged #{tags.map(&:hashtag).to_choice_sentence}"
       end
     end
 
     def assignee_summary
       if assignees.any?
-        "assigned to <mark>#{assignees.pluck(:name).to_choice_sentence}</mark>"
+        "assigned to #{assignees.pluck(:name).to_choice_sentence}"
       elsif assignments.unassigned?
         "assigned to no one"
       end
@@ -28,21 +28,21 @@ module Filter::Summarized
 
     def assigner_summary
       if assigners.any?
-        "assigned by <mark>#{assigners.pluck(:name).to_choice_sentence}</mark>"
+        "assigned by #{assigners.pluck(:name).to_choice_sentence}"
       end
     end
 
     def bucket_summary
       if buckets.any?
-        "in <mark>#{buckets.pluck(:name).to_choice_sentence}</mark>"
+        "in #{buckets.pluck(:name).to_choice_sentence}"
       else
-        "in <mark>all projects</mark>"
+        "in all projects</mark>"
       end
     end
 
     def terms_summary
       if terms.any?
-        "matching <mark>#{terms.map { |term| %Q("#{term}") }.to_sentence}</mark>"
+        "matching #{terms.map { |term| %Q("#{term}") }.to_sentence}"
       end
     end
 end
