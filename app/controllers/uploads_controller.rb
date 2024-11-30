dd45f770459e23d24605690c50d810854f714037
Jose Farias <jose@37signals.com>
2024-11-29 18:40:58 -0600

    Test UploadsController

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -1,16 +1,28 @@
 class UploadsController < ApplicationController
   include ActiveStorage::SetCurrent
 
+  before_action :set_file, only: :create
+  before_action :set_attachment, only: :show
+
   def create
-    file = params[:file]
-    blob = ActiveStorage::Blob.create_and_upload! io: file, filename: file.original_filename, content_type: file.content_type
-    @upload = Current.account.uploads_attachments.create! blob: blob
-    render :create, status: :created, formats: :json
+    @upload = Current.account.uploads_attachments.create! blob: create_blob!
   end
 
   def show
-    @attachment = ActiveStorage::Attachment.find_by! slug: "#{params[:slug]}.#{params[:format]}"
     expires_in 1.year, public: true
     redirect_to @attachment.url
   end
+
+  private
+    def set_file
+      @file = params[:file]
+    end
+
+    def set_attachment
+      @attachment = ActiveStorage::Attachment.find_by! slug: "#{params[:slug]}.#{params[:format]}"
+    end
+
+    def create_blob!
+      ActiveStorage::Blob.create_and_upload! io: @file, filename: @file.original_filename, content_type: @file.content_type
+    end
 end
