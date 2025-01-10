e17763c853d9a931adb81064d1e7c6691eefbecc
Kevin McConnell <kevin@37signals.com>
2025-01-10 15:34:58 +0000

    Use turbo:click, not click
    Otherwise Turbo falls back to a regular Browser navigation.

--- app/helpers/notifications_helper.rb
+++ app/helpers/notifications_helper.rb
@@ -7,7 +7,7 @@ module NotificationsHelper
     link_to notification.resource, id: dom_id(notification), class: "notification",
       data: {
         turbo_frame: "_top",
-        action: "notifications--readings#record",
+        action: "turbo:click->notifications--readings#record",
         notifications__readings_url_param: notification_readings_url(notification)
       }, &
   end
