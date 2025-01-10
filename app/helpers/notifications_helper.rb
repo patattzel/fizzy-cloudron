71243de163f1f34fc8915761acc5baafd4dad876
Kevin McConnell <kevin@37signals.com>
2025-01-10 17:37:54 +0000

    A partial is clearer than a helper here

--- app/helpers/notifications_helper.rb
+++ app/helpers/notifications_helper.rb
@@ -1,8 +1,4 @@
 module NotificationsHelper
-  def notification_tray
-    notification_stream_tag + notification_tray_tag
-  end
-
   def notification_tag(notification, &)
     link_to notification.resource, id: dom_id(notification), class: "notification",
       data: {
@@ -11,15 +7,4 @@ module NotificationsHelper
         notifications__readings_url_param: notification_readings_url(notification)
       }, &
   end
-
-  private
-    def notification_stream_tag
-      turbo_stream_from Current.user, :notifications
-    end
-
-    def notification_tray_tag
-      tag.div id: "notification-tray", class: "notification-tray", data: { turbo_permanent: true, controller: "notifications--readings" } do
-        turbo_frame_tag("notifications", src: notifications_path)
-      end
-    end
 end
