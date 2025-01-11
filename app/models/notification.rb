1e0d1b0aa4edef696e701a1cd47e4787594e29a7
Kevin McConnell <kevin@37signals.com>
2025-01-11 16:44:21 +0000

    Style

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,6 +1,6 @@
 class Notification < ApplicationRecord
   belongs_to :user
-  belongs_to :creator, class_name: 'User'
+  belongs_to :creator, class_name: "User"
   belongs_to :resource, polymorphic: true
 
   scope :unread, -> { where(read: false) }
