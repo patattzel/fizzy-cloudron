7c2be8733f6133326eb44b2dfc2d4a768edfb6ae
Kevin McConnell <kevin@37signals.com>
2025-01-13 13:15:20 +0000

    Style

--- app/helpers/notifications_helper.rb
+++ app/helpers/notifications_helper.rb
@@ -1,6 +1,6 @@
 module NotificationsHelper
   def notification_tag(notification, &)
     link_to notification.resource, id: dom_id(notification), class: "notification",
-      data: { turbo_frame: "_top", }, &
+      data: { turbo_frame: "_top" }, &
   end
 end
