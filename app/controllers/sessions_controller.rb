16a797682eea1dc91909675fc49aed5a8ec659ea
Kevin McConnell <kevin@37signals.com>
2025-01-13 14:20:42 +0000

    Never show login page to a logged in user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,5 @@
 class SessionsController < ApplicationController
-  allow_unauthenticated_access only: %i[ new create ]
+  require_unauthenticated_access only: %i[ new create ]
   rate_limit to: 10, within: 3.minutes, only: :create, with: -> { redirect_to new_session_path, alert: "Try again later." }
 
   before_action :require_first_run_completion
