86fdf3e4ccde7fe2ed6a40a978e61183bbcdc160
Kevin McConnell <kevin@37signals.com>
2025-01-14 14:10:24 +0000

    Add simple controller test

--- test/controllers/notifications_controller_test.rb
+++ test/controllers/notifications_controller_test.rb
@@ -1,7 +1,14 @@
 require "test_helper"
 
 class NotificationsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    sign_in_as :kevin
+  end
+
+  test "index" do
+    get notifications_url
+
+    assert_response :success
+    assert_select "div", text: /Layout is broken/
+  end
 end
