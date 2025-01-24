42dfe2e9ff267c8e3d390d1472ff6e3149dc1bc1
Jason Zimdars <jz@37signals.com>
2025-01-24 13:56:03 -0600

    Avoid leaving the tray open

--- app/helpers/notifications_helper.rb
+++ app/helpers/notifications_helper.rb
@@ -22,7 +22,7 @@ module NotificationsHelper
 
   def notification_tag(notification, &)
     link_to notification.resource, id: dom_id(notification), class: "notification border-radius",
-      data: { turbo_frame: "_top" }, &
+      data: { action: "click->dialog#close", turbo_frame: "_top" }, &
   end
 
   def notifications_next_page_link(page)
