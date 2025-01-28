bd99d8cb19b3b6d5ec8b6899b0f670f716774a94
Kevin McConnell <kevin@37signals.com>
2025-01-28 17:40:03 +0000

    Add test for events controller

---
+++ test/controllers/events_controller_test.rb
@@ -0,0 +1,25 @@
+require "test_helper"
+
+class EventsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    sign_in_as :kevin
+  end
+
+  test "index" do
+    get events_url
+
+    assert_select "div.event--cluster[style='grid-area: 9/1']" do
+      assert_select "strong", text: "Layout is broken"
+    end
+  end
+
+  test "index with a specific timezone" do
+    cookies[:timezone] = "America/New_York"
+
+    get events_url
+
+    assert_select "div.event--cluster[style='grid-area: 14/1']" do
+      assert_select "strong", text: "Layout is broken"
+    end
+  end
+end
