aa90179c366d32bc393ccba51b93d749f4de5d52
Jason Zimdars <jz@37signals.com>
2025-01-29 21:25:43 -0600

    Just use plain checkboxes
    - Style WIP

--- app/controllers/assignments/toggles_controller.rb
+++ app/controllers/assignments/toggles_controller.rb
@@ -2,12 +2,23 @@ class Assignments::TogglesController < ApplicationController
   include BubbleScoped, BucketScoped
 
   def create
-    @bubble.toggle_assignment assignee
+    new_assignee_ids = Array(params[:assignee_id])
+    current_assignees = @bubble.assignees
+
+    current_assignees.each do |assignee|
+      @bubble.toggle_assignment(assignee) unless new_assignee_ids.include?(assignee.id.to_s)
+    end
+
+    new_assignee_ids.each do |id|
+      assignee = @bucket.users.active.find(id)
+      @bubble.toggle_assignment(assignee) unless current_assignees.include?(assignee)
+    end
+
     redirect_to @bubble
   end
 
   private
     def assignee
-      @bucket.users.active.find params.expect(:assignee_id)
+      @bucket.users.active.find params[:assignee_id]
     end
 end
