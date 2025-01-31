9d98494b2aec40dfbadc2e7b28b01d1f4e30886e
Jason Zimdars <jz@37signals.com>
2025-01-31 09:59:05 -0600

    Trim color palette

--- app/models/bubble/colored.rb
+++ app/models/bubble/colored.rb
@@ -1,7 +1,7 @@
 module Bubble::Colored
   extend ActiveSupport::Concern
 
-  COLORS = %w[ #BF1B1B #ED3F1C #ED8008 #7C956B #698F9C #266ec3 #3B4B59 #5D618F #3B3633 #67695E ]
+  COLORS = %w[ #BF1B1B #ED3F1C #ED8008 #7C956B #266ec3 #3B3633 ]
 
   included do
     attribute :color, default: "#266ec3"
