d57d8db073cfecc5a22d107878d6579163bd2fb5
Jason Zimdars <jz@37signals.com>
2025-02-04 16:51:06 -0600

    Move to plain checkboxes instead of combobox

--- app/controllers/taggings/toggles_controller.rb
+++ app/controllers/taggings/toggles_controller.rb
@@ -2,16 +2,22 @@ class Taggings::TogglesController < ApplicationController
   include BubbleScoped, BucketScoped
 
   def create
-    @bubble.toggle_tag tag
-    redirect_to @bubble
-  end
+    if params[:tag_title].present?
+      @bubble.toggle_tag Current.account.tags.create!(title: params[:tag_title])
+    else
+      new_tag_ids = Array(params[:tag_id])
+      current_tags = @bubble.tags
 
-  private
-    def tag
-      if params[:tag_title]
-        Tag.new title: params[:tag_title]
-      else
-        Current.account.tags.find params.expect(:tag_id)
+      current_tags.each do |tag|
+        @bubble.toggle_tag(tag) unless new_tag_ids.include?(tag.id.to_s)
+      end
+
+      new_tag_ids.each do |id|
+        tag = Current.account.tags.find(id)
+        @bubble.toggle_tag(tag) unless current_tags.include?(tag)
       end
     end
+
+    redirect_to @bubble
+  end
 end
