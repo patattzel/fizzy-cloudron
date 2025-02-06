6253ee3f5166b57b6f2dde0abe0c3819b762d91e
Jason Zimdars <jz@37signals.com>
2025-02-06 16:45:16 -0600

    Truncation and alignment

--- app/helpers/workflows_helper.rb
+++ app/helpers/workflows_helper.rb
@@ -4,9 +4,13 @@ module WorkflowsHelper
   end
 
   def button_to_set_stage(bubble, stage)
-    button_to stage.name, bucket_bubble_stagings_path(bubble.bucket, bubble, stage_id: stage),
+    button_to bucket_bubble_stagings_path(bubble.bucket, bubble, stage_id: stage),
       method: :post, class: [ "btn full-width justify-start borderless workflow-stage", { "workflow-stage--current": stage == bubble.stage } ],
       form_class: "flex align-center gap-half",
-      data: { turbo_frame: "_top" }
+      data: { turbo_frame: "_top" } do
+        tag.span class: "overflow-ellipsis" do
+          stage.name
+        end
+      end
   end
 end
