7b6542c76b8bdc7ee3c1dfb1acf6bb44bf7b7795
Jason Zimdars <jz@37signals.com>
2025-02-21 12:12:00 -0600

    Mark all notifications as read from the index

--- app/controllers/notifications/mark_all_reads_controller.rb
+++ app/controllers/notifications/mark_all_reads_controller.rb
@@ -1,5 +1,18 @@
 class Notifications::MarkAllReadsController < ApplicationController
   def create
     Current.user.notifications.unread.read_all
+
+    set_page_and_extract_portion_from Current.user.notifications.read.ordered if request.format.turbo_stream?
+
+    Turbo::StreamsChannel.broadcast_update_to(
+      [ Current.user, :notifications ],
+      target: "notifications",
+      html: ""
+    )
+
+    respond_to do |format|
+      format.turbo_stream
+      format.html
+    end
   end
 end
