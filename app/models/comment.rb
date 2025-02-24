d7279d58170507a24a49f48b5974367226f6f2ef
Jason Zimdars <jz@37signals.com>
2025-02-24 13:24:49 -0600

    Delete referenced events when deleting comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -7,4 +7,18 @@ class Comment < ApplicationRecord
   searchable_by :body_plain_text, using: :comments_search_index, as: :body
 
   has_markdown :body
+
+  before_destroy :cleanup_events
+
+  private
+
+  def cleanup_events
+    # Delete events that reference through event_summary
+    if message&.event_summary.present?
+      Event.where(summary: message.event_summary).destroy_all
+    end
+
+    # Delete events that reference directly in particulars
+    Event.where(particulars: { comment_id: id }).destroy_all
+  end
 end
