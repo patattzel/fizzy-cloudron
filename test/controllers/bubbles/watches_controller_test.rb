9e115d0072a3c86d523d560f30b29d88f199ee1a
Kevin McConnell <kevin@37signals.com>
2025-02-26 10:58:56 +0000

    Add missing test

--- test/controllers/bubbles/watches_controller_test.rb
+++ test/controllers/bubbles/watches_controller_test.rb
@@ -1,7 +1,27 @@
 require "test_helper"
 
 class Bubbles::WatchesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    sign_in_as :kevin
+  end
+
+  test "create" do
+    bubbles(:logo).set_watching(users(:kevin), false)
+
+    assert_changes -> { bubbles(:logo).watched_by?(users(:kevin)) }, from: false, to: true do
+      post bucket_bubble_watch_url(buckets(:writebook), bubbles(:logo))
+    end
+
+    assert_redirected_to bucket_bubble_watch_url(buckets(:writebook), bubbles(:logo))
+  end
+
+  test "destroy" do
+    bubbles(:logo).set_watching(users(:kevin), true)
+
+    assert_changes -> { bubbles(:logo).watched_by?(users(:kevin)) }, from: true, to: false do
+      delete bucket_bubble_watch_url(buckets(:writebook), bubbles(:logo))
+    end
+
+    assert_redirected_to bucket_bubble_watch_url(buckets(:writebook), bubbles(:logo))
+  end
 end
