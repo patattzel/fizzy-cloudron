8a76df7ecca4531e92a434409e8bdce7f3aee479
Kevin McConnell <kevin@37signals.com>
2025-02-27 12:17:32 +0000

    Sort buckets on notification settings

--- app/controllers/notifications/settings_controller.rb
+++ app/controllers/notifications/settings_controller.rb
@@ -1,7 +1,7 @@
 module Notifications
   class SettingsController < ApplicationController
     def show
-      @buckets = Current.user.buckets.all
+      @buckets = Current.user.buckets.alphabetically
     end
   end
 end
