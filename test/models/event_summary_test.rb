169a878fac89d516ed03d6eb9ce4b3ca32155014
Jason Zimdars <jz@37signals.com>
2025-03-19 16:37:06 -0500

    Update test

--- test/models/event_summary_test.rb
+++ test/models/event_summary_test.rb
@@ -2,9 +2,9 @@ require "test_helper"
 
 class EventSummaryTest < ActiveSupport::TestCase
   test "body" do
-    assert_equal "Added by David 7 days ago. Assigned to JZ 7 days ago. David +1.", event_summaries(:logo_initial_activity).body
-    assert_equal "Assigned to Kevin 1 day ago. Kevin +2 and JZ +1.", event_summaries(:logo_second_activity).body
+    assert_equal "Added by David. Assigned to JZ. David +1.", event_summaries(:logo_initial_activity).body
+    assert_equal "Assigned to Kevin. Kevin +2 and JZ +1.", event_summaries(:logo_second_activity).body
     assert_equal "JZ +1.", event_summaries(:logo_third_activity).body
-    assert_equal "Added by Kevin 7 days ago.", event_summaries(:text_initial_activity).body
+    assert_equal "Added by Kevin.", event_summaries(:text_initial_activity).body
   end
 end
