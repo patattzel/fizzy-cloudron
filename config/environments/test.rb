a8e58fa1e794d913cf2d297079d31a31842a6c9c
Mike Dalessio <mike@37signals.com>
2025-03-20 21:58:36 -0400

    Set up tenanted Action Mailer URL helpers
    and update AR::Tenanted to a version that supports it.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -44,6 +44,9 @@ Rails.application.configure do
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
 
+  # Set host to be used by links generated in mailer templates.
+  config.action_mailer.default_url_options = { host: "%{tenant}.example.com" }
+
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
 
