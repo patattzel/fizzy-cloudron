eb456464970ae929585596be559af115bd8e365b
Andy Smith <andy@37signals.com>
2025-04-02 14:22:42 -0500

    Use mask icons everywhere

--- app/helpers/icons_helper.rb
+++
@@ -1,8 +0,0 @@
-module IconsHelper
-  def icon(name, **options)
-    classes = class_names "icon icon--#{name}", options.delete(:class)
-    options["aria-hidden"] = true
-
-    content_tag :span, "", class: classes, **options
-  end
-end
