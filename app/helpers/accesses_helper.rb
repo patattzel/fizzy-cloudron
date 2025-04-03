bae0afa83caf2e035894326e5160697de91b1d24
Jason Zimdars <jz@37signals.com>
2025-04-03 17:07:18 -0500

    Fine tune notifications states

--- app/helpers/accesses_helper.rb
+++ app/helpers/accesses_helper.rb
@@ -17,17 +17,31 @@ module AccessesHelper
     access = bucket.access_for(user)
 
     turbo_frame_tag dom_id(bucket, :involvement_button) do
-      button_to bucket_involvement_path(bucket), method: :put, class: "btn", params: { involvement: next_involvement(access.involvement) } do
-        image_tag("notification-bell-#{access.involvement.dasherize}.svg", aria: { hidden: true }, size: 24) +
-          # TODO: should the screen reader label include the state we're in, or the state the button sets it to?
-          tag.span("#{bucket.name} notification settings", class: "for-screen-reader")
+      button_to bucket_involvement_path(bucket), method: :put,
+          aria: { labelledby: dom_id(bucket, :involvement_label) },
+          class: [ "btn", { "btn--reversed": access.involvement == "watching" || access.involvement == "everything" } ],
+          params: { involvement: next_involvement(access.involvement) },
+          title: involvement_access_label(bucket, access.involvement) do
+        icon_tag("notification-bell-#{access.involvement.dasherize}") +
+          tag.span(involvement_access_label(bucket, access.involvement), class: "for-screen-reader", id: dom_id(bucket, :involvement_label))
       end
     end
   end
 
   private
     def next_involvement(involvement)
-      order = %w[ access_only watching everything ]
+      order = %w[ everything watching access_only ]
       order[(order.index(involvement.to_s) + 1) % order.size]
+    end
+
+    def involvement_access_label(bucket, involvement)
+      case involvement
+      when "access_only"
+        "Notifications are off for #{bucket.name}"
+      when "everything"
+        "Notifying me about everything in #{bucket.name}"
+      when "watching"
+        "Notifying me only about @mentions and new items in #{bucket.name}"
+      end
     end
 end
