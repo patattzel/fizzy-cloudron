7abacd4da9911c2563c4848de278842f2098c35f
David Heinemeier Hansson <david@37signals.com>
2025-04-05 15:15:48 +0200

    Shape up pinning API and broadcast in the controller

--- app/controllers/bubbles/pins_controller.rb
+++ app/controllers/bubbles/pins_controller.rb
@@ -5,12 +5,25 @@ class Bubbles::PinsController < ApplicationController
   end
 
   def create
-    @bubble.set_pinned(Current.user, true)
+    pin = @bubble.pin_by Current.user
+
+    broadcast_new pin
     redirect_to bucket_bubble_pin_path(@bucket, @bubble)
   end
 
   def destroy
-    @bubble.set_pinned(Current.user, false)
+    pin = @bubble.unpin_by Current.user
+
+    broadcast_removed pin
     redirect_to bucket_bubble_pin_path(@bucket, @bubble)
   end
+
+  private
+    def broadcast_new(pin)
+      pin.broadcast_prepend_later_to [ Current.user, :pins ], target: "pins", partial: "my/pins/pin"
+    end
+
+    def broadcast_removed(pin)
+      pin.broadcast_remove_to [ Current.user, :pins ]
+    end
 end
