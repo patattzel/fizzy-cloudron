0b19bc16471b87a25741bcf2650d118461e1ad13
David Heinemeier Hansson <david@37signals.com>
2025-04-05 15:55:11 +0200

    Emphasize where this pin is showing up

--- app/controllers/bubbles/pins_controller.rb
+++ app/controllers/bubbles/pins_controller.rb
@@ -7,23 +7,23 @@ class Bubbles::PinsController < ApplicationController
   def create
     pin = @bubble.pin_by Current.user
 
-    broadcast_new pin
+    broadcast_my_new pin
     redirect_to bubble_pin_path(@bubble)
   end
 
   def destroy
     pin = @bubble.unpin_by Current.user
 
-    broadcast_removed pin
+    broadcast_my_removed pin
     redirect_to bubble_pin_path(@bubble)
   end
 
   private
-    def broadcast_new(pin)
+    def broadcast_my_new(pin)
       pin.broadcast_prepend_later_to [ Current.user, :pins ], target: "pins", partial: "my/pins/pin"
     end
 
-    def broadcast_removed(pin)
+    def broadcast_my_removed(pin)
       pin.broadcast_remove_to [ Current.user, :pins ]
     end
 end
