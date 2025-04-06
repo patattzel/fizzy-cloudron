9bac64b6ba99e94d610e0bb9a9d9b0e8abc1300b
David Heinemeier Hansson <david@37signals.com>
2025-04-06 13:29:43 +0200

    This is not just param extraction, we sanitize too

--- app/controllers/bubbles/taggings_controller.rb
+++ app/controllers/bubbles/taggings_controller.rb
@@ -6,11 +6,11 @@ class Bubbles::TaggingsController < ApplicationController
   end
 
   def create
-    @bubble.toggle_tag_with(tag_title_param)
+    @bubble.toggle_tag_with(sanitized_tag_title_param)
   end
 
   private
-    def tag_title_param
+    def sanitized_tag_title_param
       params.required(:tag_title).strip.gsub(/\A#/, "")
     end
 end
