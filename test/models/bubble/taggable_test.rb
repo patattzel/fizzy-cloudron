ce08fc0ed3417cfd82573bbf561e0082a88feba9
Jorge Manrubia <jorge@37signals.com>
2025-04-07 11:36:34 +0200

    Add unique constraint for keeping tags unique on a per-account basis

--- test/models/bubble/taggable_test.rb
+++ test/models/bubble/taggable_test.rb
@@ -16,12 +16,4 @@ class Bubble::TaggableTest < ActiveSupport::TestCase
       @bubble.toggle_tag_with "ruby"
     end
   end
-
-  test "ignore case when toggling tags" do
-    @bubble.toggle_tag_with "ruby"
-
-    assert_difference -> { @bubble.tags.count }, -1 do
-      @bubble.toggle_tag_with "Ruby"
-    end
-  end
 end
