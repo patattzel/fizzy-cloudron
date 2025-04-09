362d337a9a290fa73b76145914af3db775a69508
Jason Zimdars <jz@37signals.com>
2025-04-09 15:42:33 -0500

    Move concern out from under `Card` and rename
    `Colored` is a little icky

--- app/models/card/colored.rb
+++
@@ -1,9 +0,0 @@
-module Card::Colored
-  extend ActiveSupport::Concern
-
-  COLORS = %w[ #b7462b #ff63a8 #eb7a32 #6ac967 #2c6da8 #663251  ]
-
-  included do
-    attribute :color, default: "#2c6da8"
-  end
-end
