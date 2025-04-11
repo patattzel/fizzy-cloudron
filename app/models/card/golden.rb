160e08d77c6cca8f98ef29d7b183e312d9de65e7
Jorge Manrubia <jorge@37signals.com>
2025-04-11 09:49:34 +0200

    Add comment

--- app/models/card/golden.rb
+++ app/models/card/golden.rb
@@ -8,6 +8,7 @@ module Card::Golden
 
     scope :with_golden_first, -> do
       left_outer_joins(:goldness).tap do |relation|
+        # Make sure it can be layered in with other orderings taking precedence.
         relation.order_values.unshift("card_goldnesses.id IS NULL")
       end
     end
