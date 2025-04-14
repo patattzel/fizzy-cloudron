patattzel/fizzy-cloudron

commit 51cf56a6b456a4cb3f9f0f411e975bd1a342f3e1

Style

app/controllers/notifications_controller.rb
@@ -1,10 +1,10 @@
 class NotificationsController < ApplicationController
   def index
-    set_page_and_extract_portion_from Current.user.notifications.read.ordered
-
     if @page.first?
       @unread = Current.user.notifications.unread.ordered
     end
+
+    set_page_and_extract_portion_from Current.user.notifications.read.ordered
   end
 
   def mark_read

commit a53e3cfdd5425dfe0ac401b159453c761b8d4aea

Source order seems to be significant
Regressed in 51cf56a6b456a4cb3f9f0f411e975bd1a342f3e1

app/controllers/notifications_controller.rb
@@ -1,10 +1,10 @@
 class NotificationsController < ApplicationController
   def index
+    set_page_and_extract_portion_from Current.user.notifications.read.ordered
+
     if @page.first?
       @unread = Current.user.notifications.unread.ordered
     end
-
-    set_page_and_extract_portion_from Current.user.notifications.read.ordered
   end
 
   def mark_read