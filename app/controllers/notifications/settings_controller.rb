60ca6bef9d21ec59d034a3f6c568a905a22ebc5c
David Heinemeier Hansson <david@37signals.com>
2025-04-15 16:36:40 +0200

    We can just set the namespace directly in the class name

--- app/controllers/notifications/settings_controller.rb
+++ app/controllers/notifications/settings_controller.rb
@@ -1,7 +1,5 @@
-module Notifications
-  class SettingsController < ApplicationController
-    def show
-      @collections = Current.user.collections.alphabetically
-    end
+class Notifications::SettingsController < ApplicationController
+  def show
+    @collections = Current.user.collections.alphabetically
   end
 end
