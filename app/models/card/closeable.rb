237ead7c88ed82799a01e791194dde2485c4df3d
Andy Smith <andy@37signals.com>
2025-04-15 13:24:40 -0500

    Remind 7 days before auto close

--- app/models/card/closeable.rb
+++ app/models/card/closeable.rb
@@ -2,7 +2,7 @@ module Card::Closeable
   extend ActiveSupport::Concern
 
   AUTO_CLOSE_AFTER = 30.days
-  AUTO_CLOSE_REMINDER = 22.days
+  AUTO_CLOSE_REMINDER = 7.days
 
   included do
     has_one :closure, dependent: :destroy
