74c496ad71ba1de673b2f258357c8e0d4863342a
David Heinemeier Hansson <david@37signals.com>
2025-04-16 16:21:31 +0200

    Dont turbo stream when coming from a read_all redirect

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -5,5 +5,10 @@ class NotificationsController < ApplicationController
     if @page.first?
       @unread = Current.user.notifications.unread.ordered
     end
+
+    respond_to do |format|
+      format.turbo_stream if current_page_param
+      format.html
+    end
   end
 end
