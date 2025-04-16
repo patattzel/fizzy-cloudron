344a4b37f13a02f259ef2d09aafe853d89618003
David Heinemeier Hansson <david@37signals.com>
2025-04-16 15:34:28 +0200

    Inline anemic method

--- app/controllers/cards/readings_controller.rb
+++ app/controllers/cards/readings_controller.rb
@@ -2,12 +2,7 @@ class Cards::ReadingsController < ApplicationController
   include CardScoped
 
   def create
-    mark_card_notifications_read
+    Current.user.notifications.unread.where(card: @card).read_all
     @notifications = Current.user.notifications.unread.ordered.limit(20)
   end
-
-  private
-    def mark_card_notifications_read
-      Current.user.notifications.unread.where(card: @card).read_all
-    end
 end
