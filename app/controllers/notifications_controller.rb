6d922ea66f63f5d4ea6f2128eb29bb7c88b3c340
David Heinemeier Hansson <david@37signals.com>
2025-04-16 16:58:03 +0200

    More natural to fetch that which will be shown first

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,10 +1,10 @@
 class NotificationsController < ApplicationController
   def index
-    set_page_and_extract_portion_from Current.user.notifications.read.ordered
-
-    if @page.first?
+    unless current_page_param
       @unread = Current.user.notifications.unread.ordered
     end
+
+    set_page_and_extract_portion_from Current.user.notifications.read.ordered
 
     respond_to do |format|
       format.turbo_stream if current_page_param # Allows read-all action to side step pagination
