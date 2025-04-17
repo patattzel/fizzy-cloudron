3d158bf344cdca5fb66ec8868d06a1280b554ae0
David Heinemeier Hansson <david@37signals.com>
2025-04-17 14:29:12 +0200

    Clearer about where these are being sent

--- app/controllers/cards/pins_controller.rb
+++ app/controllers/cards/pins_controller.rb
@@ -6,20 +6,20 @@ class Cards::PinsController < ApplicationController
 
   def create
     pin = @card.pin_by Current.user
-    broadcast_my_new pin
+    broadcast_add_to_tray pin
   end
 
   def destroy
     pin = @card.unpin_by Current.user
-    broadcast_my_removed pin
+    broadcast_remove_from_tray pin
   end
 
   private
-    def broadcast_my_new(pin)
+    def broadcast_add_to_tray(pin)
       pin.broadcast_prepend_later_to [ Current.user, :pins ], target: "pins", partial: "my/pins/pin"
     end
 
-    def broadcast_my_removed(pin)
+    def broadcast_remove_from_tray(pin)
       pin.broadcast_remove_to [ Current.user, :pins ]
     end
 end
