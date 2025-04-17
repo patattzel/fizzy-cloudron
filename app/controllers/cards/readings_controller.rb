00b849012d52be8b012802393fad95ae83a94d66
David Heinemeier Hansson <david@37signals.com>
2025-04-17 11:39:05 +0200

    Allow for multiple notifications to be read
    cc @kevinmcconnell

--- app/controllers/cards/readings_controller.rb
+++ app/controllers/cards/readings_controller.rb
@@ -2,7 +2,7 @@ class Cards::ReadingsController < ApplicationController
   include CardScoped
 
   def create
-    Current.user.notifications.unread.where(card: @card).read_all
-    @notifications = Current.user.notifications.unread.ordered.limit(20)
+    @notifications = Current.user.notifications.where(card: @card)
+    @notifications.each(&:read)
   end
 end
