586505096045066075e05f9940e9988ec65aac3b
David Heinemeier Hansson <david@37signals.com>
2025-04-18 16:42:35 +0200

    Style

--- app/helpers/workflows_helper.rb
+++ app/helpers/workflows_helper.rb
@@ -1,13 +1,10 @@
 module WorkflowsHelper
   def button_to_set_stage(card, stage)
-    button_to card_stagings_path(card, stage_id: stage),
+    button_to \
+        tag.span(stage.name, class: "overflow-ellipsis"), card_stagings_path(card, stage_id: stage),
         method: :post, class: [ "btn justify-start workflow-stage txt-uppercase workflow-stage", { "workflow-stage--current": stage == card.stage } ],
         form_class: "flex align-center gap-half",
-        data: { turbo_frame: "_top" } do
-      tag.span class: "overflow-ellipsis" do
-        stage.name
-      end
-    end
+        data: { turbo_frame: "_top" }
   end
 
   def stage_color(stage)
