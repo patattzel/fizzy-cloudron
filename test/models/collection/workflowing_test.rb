684a12fb3a09e447f1b063f3163f2f3a18789ed8
David Heinemeier Hansson <david@37signals.com>
2025-04-22 13:05:39 +0200

    Test and fix setting workflow to nothing

--- test/models/collection/workflowing_test.rb
+++ test/models/collection/workflowing_test.rb
@@ -5,4 +5,9 @@ class Collection::WorkflowingTest < ActiveSupport::TestCase
     collections(:writebook).update! workflow: workflows(:on_call)
     assert_equal [ collections(:writebook).initial_workflow_stage ], collections(:writebook).cards.reload.collect(&:stage).uniq
   end
+
+  test "remove stage from cards if workflow is removed" do
+    collections(:writebook).update! workflow: nil
+    assert_equal [], collections(:writebook).cards.reload.collect(&:stage).compact
+  end
 end
