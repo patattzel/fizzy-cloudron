eb8e68c2d5a973b584d80e0dfc616d609a5089ac
David Heinemeier Hansson <david@37signals.com>
2025-04-22 11:53:16 +0200

    Unused

--- app/models/colorable.rb
+++ app/models/colorable.rb
@@ -3,8 +3,4 @@ module Colorable
 
   COLORS = %w[ #3b3633 #67695e #eb7a32 #bf7c2b #c09c6f #746b1e #2c6da8 #5d618f #663251 #ff63a8 ]
   DEFAULT_COLOR = "#2c6da8"
-
-  included do
-    attribute :color, default: DEFAULT_COLOR
-  end
 end
