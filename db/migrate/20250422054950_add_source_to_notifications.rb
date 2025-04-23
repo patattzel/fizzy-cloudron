3064bb2706fa20a0185294d18c8b49824bd4e736
Jorge Manrubia <jorge@37signals.com>
2025-04-23 18:45:45 +0200

    FIx migration

--- db/migrate/20250422054950_add_source_to_notifications.rb
+++ db/migrate/20250422054950_add_source_to_notifications.rb
@@ -3,7 +3,7 @@ class AddSourceToNotifications < ActiveRecord::Migration[8.1]
     add_reference :notifications, :source, polymorphic: true, index: true
 
     execute <<~SQL
-      update notifications set source_type = 'Event'
+      update notifications set source_type = 'Event', source_id = event_id;
     SQL
 
     change_column_null :notifications, :source_type, false
