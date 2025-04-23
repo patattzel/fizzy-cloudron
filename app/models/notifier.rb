2ddb58fb5ebbd05279fe5d112222d1695fca70eb
Jorge Manrubia <jorge@37signals.com>
2025-04-23 12:44:36 +0200

    Remove identation

--- app/models/notifier.rb
+++ app/models/notifier.rb
@@ -4,10 +4,10 @@ class Notifier
   class << self
     def for(source)
       case source
-        when ::Event
-          Notifier::Event.new(source)
-        when ::Mention
-          Notifier::Mention.new(source)
+      when ::Event
+        Notifier::Event.new(source)
+      when ::Mention
+        Notifier::Mention.new(source)
       end
     end
   end
