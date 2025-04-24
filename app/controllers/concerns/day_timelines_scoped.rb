7c9cdd937edd0a798ccc6a4e870276e2fa5d7f17
Jorge Manrubia <jorge@37signals.com>
2025-04-24 18:53:35 +0200

    Add comment about something that is pending

--- app/controllers/concerns/day_timelines_scoped.rb
+++ app/controllers/concerns/day_timelines_scoped.rb
@@ -10,12 +10,13 @@ module DayTimelinesScoped
   private
     def restore_collections_filter
       cookies.delete(:collection_filter) if params[:clear_filter]
-      set_collections_filter if cookies[:collection_filter].present?
+      # TODO: This is not working as intended yet, I'll fix
+      set_collections_filter_from_cookie
+      cookies[:collection_filter] = @filter.collection_ids.join(",")
     end
 
-    def set_collections_filter
-      @filter.collection_ids = cookies[:collection_filter].split(",")
-      cookies[:collection_filter] = @filter.collection_ids.join(",")
+    def set_collections_filter_from_cookie
+      @filter.collection_ids = cookies[:collection_filter].split(",") if cookies[:collection_filter].present?
     end
 
     def set_day_timeline
