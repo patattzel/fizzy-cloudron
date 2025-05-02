7693526ae84f3acfee61923cb75987dc3bc3be5c
Jason Zimdars <jz@37signals.com>
2025-05-02 16:04:32 +0200

    Stub stalled setting, adjust auto-close options

--- app/helpers/collections_helper.rb
+++ app/helpers/collections_helper.rb
@@ -1,11 +1,20 @@
 module CollectionsHelper
   def collection_auto_close_options
     [
+      [ "7 days", 7.days ],
       [ "30 days", 30.days ],
-      [ "60 days", 60.days ],
       [ "90 days", 90.days ],
-      [ "6 months", 180.days ],
-      [ "1 year", 365.days ],
+      [ "Never", nil ]
+    ]
+  end
+
+  def collection_stalled_options
+    [
+      [ "1 day", 1.days ],
+      [ "2 days", 2.days ],
+      [ "3 days", 3.days ],
+      [ "7 days", 7.days ],
+      [ "30 days", 30.days ],
       [ "Never", nil ]
     ]
   end
