51fc04e29afafd407d27a7fc7e8083a3f1b7338b
Andy Smith <andy@37signals.com>
2025-05-05 13:34:35 -0500

    Use variables for card colors

--- app/models/card/colored.rb
+++ app/models/card/colored.rb
@@ -1,8 +1,18 @@
 module Card::Colored
   extend ActiveSupport::Concern
 
-  COLORS = %w[ #67695e #eb7a32 #bf7c2b #c09c6f #746b1e #2c6da8 #5d618f #663251 #ff63a8 ]
-  DEFAULT_COLOR = "#2c6da8"
+  COLORS = %w[
+    var(--color-card-1)
+    var(--color-card-2)
+    var(--color-card-3)
+    var(--color-card-4)
+    var(--color-card-5)
+    var(--color-card-6)
+    var(--color-card-7)
+    var(--color-card-8)
+    var(--color-card-9)
+  ]
+  DEFAULT_COLOR = COLORS[5]
 
   def color
     color_from_stage || DEFAULT_COLOR
