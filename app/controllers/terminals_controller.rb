560b3ed3ba33d1ef0065198cbb070731c052f019
Jorge Manrubia <jorge@37signals.com>
2025-05-06 08:56:56 +0200

    Remove old code

--- app/controllers/terminals_controller.rb
+++
@@ -1,9 +0,0 @@
-class TerminalsController < ApplicationController
-  def show
-    @events = Current.user.accessible_events.chronologically.reverse_order.limit(20)
-  end
-
-  def edit
-    @filter = Current.user.filters.from_params params.permit(*Filter::PERMITTED_PARAMS)
-  end
-end
