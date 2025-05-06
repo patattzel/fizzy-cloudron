dc099c14501f807214ab24f58a1fe7004309a3c0
Jorge Manrubia <jorge@37signals.com>
2025-05-06 07:29:59 +0200

    Don't filter for blank queries

--- app/models/command/search.rb
+++ app/models/command/search.rb
@@ -2,7 +2,7 @@ class Command::Search < Command
   store_accessor :data, :query
 
   def execute
-    redirect_to cards_path("terms[]": query)
+    redirect_to cards_path("terms[]": query.presence)
   end
 
   def description
