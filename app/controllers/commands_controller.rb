6831ecddb96b8a116296903bbf6af7ac196ab625
Jorge Manrubia <jorge@37signals.com>
2025-05-06 11:09:07 +0200

    Not used

--- app/controllers/commands_controller.rb
+++ app/controllers/commands_controller.rb
@@ -30,8 +30,4 @@ class CommandsController < ApplicationController
     def parsing_context
       Command::Parser::Context.new(Current.user, url: request.referrer)
     end
-
-    def card_ids_from_header
-      request.headers["X-Cards"].to_s.split(",").map(&:to_i) if request.headers["X-Card-Ids"]
-    end
 end
