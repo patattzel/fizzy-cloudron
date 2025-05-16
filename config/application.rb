394bb3d31b77e55699814ebd999775bd134d69df
Jorge Manrubia <jorge@37signals.com>
2025-05-16 16:17:54 +0200

    Move extension configuration to config file
    Kudos to Mike for the tip

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,7 @@
 require_relative "boot"
 
 require "rails/all"
+require "sqlite_vec" # Referenced in database.yml
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
