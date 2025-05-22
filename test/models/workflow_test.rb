c65c1b7d77fb59e5bc902cf6feaae86b5ee1e0e1
Jason Zimdars <jz@37signals.com>
2025-05-22 17:02:32 -0500

    Adjust for change from Array strings to hashes

--- test/models/workflow_test.rb
+++ test/models/workflow_test.rb
@@ -3,6 +3,6 @@ require "test_helper"
 class WorkflowTest < ActiveSupport::TestCase
   test "create with default stages" do
     workflow = Workflow.create name: "My New Workflow"
-    assert_equal Workflow::DEFAULT_STAGES.sort, workflow.stages.collect(&:name).sort
+    assert_equal Workflow::DEFAULT_STAGES.map { it[:name] }.sort, workflow.stages.collect(&:name).sort
   end
 end
