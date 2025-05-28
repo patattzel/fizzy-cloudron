2c3cd853b871cc511a545c94a4380be1e1cdb9b9
Jorge Manrubia <jorge@37signals.com>
2025-05-28 22:36:06 +0200

    Bye house!

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,7 +5,6 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin "@rails/request.js", to: "@rails--request.js" # @0.0.11
-pin "house", to: "house.min.js"
 
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin_all_from "app/javascript/helpers", under: "helpers"
