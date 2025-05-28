e045cddea5bb1d2c9f1554c7dce755a3e06e1d81
Jason Zimdars <jz@37signals.com>
2025-05-28 15:32:00 -0500

    Update tests

--- test/models/card/eventable/system_commenter_test.rb
+++ test/models/card/eventable/system_commenter_test.rb
@@ -7,7 +7,7 @@ class Card::Eventable::SystemCommenterTest < ActiveSupport::TestCase
   end
 
   test "card_assigned" do
-    assert_system_comment "Assigned to Kevin" do
+    assert_system_comment "David assigned this to Kevin." do
       @card.toggle_assignment users(:kevin)
     end
   end
@@ -16,7 +16,7 @@ class Card::Eventable::SystemCommenterTest < ActiveSupport::TestCase
     @card.toggle_assignment users(:kevin)
     @card.comments.destroy_all # To skip deduplication logic
 
-    assert_system_comment "Unassigned from Kevin" do
+    assert_system_comment "David unassigned from Kevin" do
       @card.toggle_assignment users(:kevin)
     end
   end
@@ -28,32 +28,14 @@ class Card::Eventable::SystemCommenterTest < ActiveSupport::TestCase
   end
 
   test "card_closed" do
-    assert_system_comment "Closed by David" do
+    assert_system_comment "Closed as “Done” by David" do
       @card.close
     end
   end
 
   test "card_title_changed" do
-    assert_system_comment "David changed title from 'The text is too small' to 'Make text larger'" do
+    assert_system_comment "David changed the title from 'The text is too small' to 'Make text larger'." do
       @card.update! title: "Make text larger"
-    end
-  end
-
-  test "don't duplicate comments for the same kind of events" do
-    @card.change_stage_to workflow_stages(:qa_in_progress)
-
-    assert_no_difference -> { @card.comments.count } do
-      @card.reload.change_stage_to workflow_stages(:qa_on_hold)
-      assert_match /David moved this to 'On hold'/i, @card.comments.last.body.to_plain_text
-    end
-  end
-
-  test "don't duplicate comments for related events" do
-    @card.toggle_assignment users(:kevin)
-
-    assert_no_difference -> { @card.comments.count } do
-      @card.reload.toggle_assignment users(:kevin)
-      assert_match /Unassigned from Kevin/i, @card.comments.last.body.to_plain_text
     end
   end
 
