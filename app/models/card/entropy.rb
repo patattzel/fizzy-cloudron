a3cc775977740a5b169aedcd6abe9f6bf7fbdef5
Jorge Manrubia <jorge@37signals.com>
2025-06-04 12:58:17 +0200

    Simpler

--- app/models/card/entropy.rb
+++ app/models/card/entropy.rb
@@ -7,8 +7,7 @@ module Card::Entropy
   included do
     scope :entropic_by, ->(period_name) do
       left_outer_joins(collection: :entropy_configuration)
-        .where("last_active_at <= DATETIME('now', '-' || COALESCE(entropy_configurations.#{period_name}, (?)) || ' seconds')",
-        Entropy::Configuration.where(id: Entropy::Configuration.default).select(period_name).limit(1))
+        .where("last_active_at <= DATETIME('now', '-' || COALESCE(entropy_configurations.#{period_name}, (?)) || ' seconds')", Entropy::Configuration.default.public_send(period_name))
     end
 
     scope :stagnated, -> { doing.entropic_by(:auto_reconsider_period) }
