635a4d7958e8e365b5ca9dc7aee3611cadb205d5
Jason Zimdars <jz@37signals.com>
2025-06-04 20:28:33 -0500

    We're not using this right now

--- app/helpers/collections_helper.rb
+++ app/helpers/collections_helper.rb
@@ -9,16 +9,4 @@ module CollectionsHelper
       [ "365", 365.days ]
     ]
   end
-
-  def collection_stalled_options
-    [
-      [ "1 day", 1.days ],
-      [ "2 days", 2.days ],
-      [ "3 days", 3.days ],
-      [ "7 days", 7.days ],
-      [ "14 days", 14.days ],
-      [ "30 days", 30.days ],
-      [ "365 days", 365.days ]
-    ]
-  end
 end
