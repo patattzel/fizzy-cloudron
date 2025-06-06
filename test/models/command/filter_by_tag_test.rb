e7e452637ae12ae1f22bd3c983717e2b5739a8e4
Jorge Manrubia <jorge@37signals.com>
2025-06-06 08:25:49 +0200

    Fix tests after removing indexed_by param

--- test/models/command/filter_by_tag_test.rb
+++ test/models/command/filter_by_tag_test.rb
@@ -10,6 +10,6 @@ class Command::FilterByCardTest < ActionDispatch::IntegrationTest
   test "redirect to the cards index filtering by cards" do
     result = execute_command "##{@tag.title}"
 
-    assert_equal cards_path(indexed_by: "newest", tag_ids: [ @tag.id ]), result.url
+    assert_equal cards_path(tag_ids: [ @tag.id ]), result.url
   end
 end
