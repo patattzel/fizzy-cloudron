35571461d12c5d3ec03ac761218f92c433bcd948
Jorge Manrubia <jorge@37signals.com>
2025-06-24 12:45:15 +0200

    Extract common concern

--- app/controllers/searches/queries_controller.rb
+++ app/controllers/searches/queries_controller.rb
@@ -1,11 +1,8 @@
 class Searches::QueriesController < ApplicationController
+  include Search::QueryTermsScoped
+
   def create
-    Current.user.remember_search(terms_params)
+    Current.user.remember_search(@query_terms)
     head :ok
   end
-
-  private
-    def terms_params
-      params[:terms]
-    end
 end
--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,13 +1,8 @@
 class SearchesController < ApplicationController
-  before_action :set_query_terms
+  include Search::QueryTermsScoped
 
   def show
     @search_results = Current.user.search(@query_terms).limit(50)
     @recent_search_queries = Current.user.search_queries.order(created_at: :desc).limit(10)
   end
-
-  private
-    def set_query_terms
-      @query_terms = params[:q]
-    end
 end
