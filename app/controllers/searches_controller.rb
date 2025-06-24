5e4bf1e5656031e498377cb069fcf50dd25f0ab7
Jorge Manrubia <jorge@37signals.com>
2025-06-24 12:36:10 +0200

    Extract query_terms to an instance variable

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,11 +1,13 @@
 class SearchesController < ApplicationController
+  before_action :set_query_terms
+
   def show
-    @search_results = Current.user.search(query_param).limit(50)
+    @search_results = Current.user.search(@query_terms).limit(50)
     @recent_search_queries = Current.user.search_queries.order(created_at: :desc).limit(10)
   end
 
   private
-    def query_param
-      params[:q]
+    def set_query_terms
+      @query_terms = params[:q]
     end
 end
