bc7a90756e685c89cb449e4bc168bc7d9dad17f7
Jorge Manrubia <jorge@37signals.com>
2025-07-01 09:42:22 +0200

    Tidy up code to normalize text, extract text helpers

--- app/javascript/controllers/filter_controller.js
+++ app/javascript/controllers/filter_controller.js
@@ -1,5 +1,6 @@
 import { Controller } from "@hotwired/stimulus"
 import { debounce } from "helpers/timing_helpers"
+import { filterMatches } from "helpers/text_helpers"
 
 export default class extends Controller {
   static targets = [ "input", "item" ]
@@ -10,7 +11,7 @@ export default class extends Controller {
 
   filter() {
     this.itemTargets.forEach(item => {
-      if (this.#convertDiacritics(item.innerText).toLowerCase().includes(this.inputTarget.value.toLowerCase())) {
+      if (filterMatches(item.innerText, this.inputTarget.value)) {
         item.removeAttribute("hidden")
       } else {
         item.toggleAttribute("hidden", true)
@@ -18,9 +19,5 @@ export default class extends Controller {
     })
 
     this.dispatch("changed")
-  }
-
-  #convertDiacritics(text) {
-    return text.normalize("NFD").replace(/[\u0300-\u036f]/g, "")
   }
 }
