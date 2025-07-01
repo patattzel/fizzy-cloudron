bb27bdb93bae95cf2bb00d7fc16bcb33929459ef
Jorge Manrubia <jorge@37signals.com>
2025-07-01 18:31:51 +0200

    Remove caching so that it doesn't interfere when working on design

--- app/controllers/prompts/commands_controller.rb
+++ app/controllers/prompts/commands_controller.rb
@@ -1,9 +1,9 @@
 class Prompts::CommandsController < ApplicationController
   def index
-    @commands = []
-
-    if stale? etag: @commands
-      render layout: false
-    end
+    # @commands = []
+    #
+    # if stale? etag: @commands
+    render layout: false
+    # end
   end
 end
