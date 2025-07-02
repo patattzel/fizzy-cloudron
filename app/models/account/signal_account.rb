674210b30dbc5d7928097e86c2b87c67f36ff2a7
Mike Dalessio <mike@37signals.com>
2025-07-02 11:59:21 -0400

    Remove local authentication code

--- app/models/account/signal_account.rb
+++ app/models/account/signal_account.rb
@@ -2,10 +2,8 @@ module Account::SignalAccount
   extend ActiveSupport::Concern
 
   included do
-    unless Rails.application.config.x.local_authentication
-      # TODO: remove the "optional: true" once we've populated the accounts properly
-      belongs_to :signal_account, class_name: "SignalId::Account", primary_key: :queenbee_id, foreign_key: :queenbee_id, optional: true
-    end
+    # TODO: remove the "optional: true" once we've populated the accounts properly
+    belongs_to :signal_account, class_name: "SignalId::Account", primary_key: :queenbee_id, foreign_key: :queenbee_id, optional: true
   end
 
   class_methods do
