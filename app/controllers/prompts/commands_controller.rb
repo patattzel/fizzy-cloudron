c0cfb7750ce9ce395226b5af5f8868c8b867f3ff
Jorge Manrubia <jorge@37signals.com>
2025-07-02 21:26:24 +0200

    Format

--- app/controllers/prompts/commands_controller.rb
+++ app/controllers/prompts/commands_controller.rb
@@ -1,16 +1,16 @@
 class Prompts::CommandsController < ApplicationController
   def index
     @commands = [
-      ["/add_card", "Create a new card", "/add_card "],
-      ["/assign", "Assign cards to users", "/assign @"],
-      ["/clear", "Clear current filters", "/clear"],
-      ["/close", "Close cards with a reason", "/close "],
-      ["/consider", "Reconsider cards", "/consider"],
-      ["/do", "Move cards to doing", "/do"],
-      ["/reconsider", "Move cards to reconsidering", "/reconsider"],
-      ["/search", "Search cards and comments", "/search "],
-      ["/tag", "Tag selected cards", "/tag #"],
-      ["/stage", "Set cards to a workflow stage", "/stage "]
+      [ "/add_card", "Create a new card", "/add_card " ],
+      [ "/assign", "Assign cards to users", "/assign @" ],
+      [ "/clear", "Clear current filters", "/clear" ],
+      [ "/close", "Close cards with a reason", "/close " ],
+      [ "/consider", "Reconsider cards", "/consider" ],
+      [ "/do", "Move cards to doing", "/do" ],
+      [ "/reconsider", "Move cards to reconsidering", "/reconsider" ],
+      [ "/search", "Search cards and comments", "/search " ],
+      [ "/tag", "Tag selected cards", "/tag #" ],
+      [ "/stage", "Set cards to a workflow stage", "/stage " ]
     ]
 
     if stale? etag: @commands
