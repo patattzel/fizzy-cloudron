0e37614ae26a65b54cb48c25c52a531882dcbb49
Jason Zimdars <jz@37signals.com>
2025-07-23 17:26:01 -0500

    Test the destroy action

--- test/controllers/notifications/readings_controller_test.rb
+++ test/controllers/notifications/readings_controller_test.rb
@@ -12,6 +12,16 @@ class Notifications::ReadingsControllerTest < ActionDispatch::IntegrationTest
     end
   end
 
+  test "destroy" do
+    notification = notifications(:logo_published_kevin)
+    notification.read # Mark as read first
+
+    assert_changes -> { notification.reload.read? }, from: true, to: false do
+      delete read_notification_path(notification, format: :turbo_stream)
+      assert_response :success
+    end
+  end
+
   test "create all" do
     assert_changes -> { notifications(:logo_published_kevin).reload.read? }, from: false, to: true do
       assert_changes -> { notifications(:layout_commented_kevin).reload.read? }, from: false, to: true do
