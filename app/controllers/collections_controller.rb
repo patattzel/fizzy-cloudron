453dedebe1abeb87a726eddf5bd9befa52a95677
Mike Dalessio <mike@37signals.com>
2025-08-07 14:14:18 -0400

    Introduce turbo_stream_flash helper and use it for collection edit
    The collections/{entropy,publications,workflows} controllers all
    respond via turbo stream, and now also provide a flash message.

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -19,7 +19,7 @@ class CollectionsController < ApplicationController
     @collection.update! collection_params
     @collection.accesses.revise granted: grantees, revoked: revokees if grantees_changed?
 
-    redirect_to edit_collection_path(@collection), notice: "Changes saved"
+    redirect_to edit_collection_path(@collection), notice: "Saved"
   end
 
   def destroy
