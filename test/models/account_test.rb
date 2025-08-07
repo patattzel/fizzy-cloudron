2049f25592d7d122388167d33d69d372a849b4fb
Mike Dalessio <mike@37signals.com>
2025-08-07 09:44:58 -0400

    Prefer Account#slug to Account.script_name
    Caching the script name in a class instance variable seemed like a bug
    to me.

--- test/models/account_test.rb
+++ test/models/account_test.rb
@@ -1,4 +1,8 @@
 require "test_helper"
 
 class AccountTest < ActiveSupport::TestCase
+  test "slug" do
+    account = Account.sole
+    assert_equal "/#{account.queenbee_id}", account.slug
+  end
 end
