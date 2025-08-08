a3a3a2fb3a59cdbf684ab49f09fc79c6d6a20535
Mike Dalessio <mike@37signals.com>
2025-08-08 14:18:08 -0400

    Well, well, well, if it isn't the consequences of my own actions

--- config/initializers/sanitization.rb
+++ config/initializers/sanitization.rb
@@ -1,7 +1,8 @@
 Rails.application.config.after_initialize do
-  Rails::HTML5::SafeListSanitizer.allowed_tags.merge(%w[ s table tr td th thead tbody details summary video source])
+  Rails::HTML5::SafeListSanitizer.allowed_tags.merge(%w[ s table tr td th thead tbody details summary video source ])
   Rails::HTML5::SafeListSanitizer.allowed_attributes.merge(%w[ data-turbo-frame controls type width data-action data-lightbox-target data-lightbox-url-value ])
 
-  ActionText::ContentHelper.allowed_tags = Rails::HTML5::SafeListSanitizer.allowed_tags
-  ActionText::ContentHelper.allowed_attributes = Rails::HTML5::SafeListSanitizer.allowed_attributes
+  # ugh, see https://github.com/rails/rails/issues/54478 which I need to fix upstream --mike
+  ActionText::ContentHelper.allowed_tags = Rails::HTML5::SafeListSanitizer.allowed_tags + [ ActionText::Attachment.tag_name, "figure", "figcaption" ]
+  ActionText::ContentHelper.allowed_attributes = Rails::HTML5::SafeListSanitizer.allowed_attributes + ActionText::Attachment::ATTRIBUTES
 end
