96332542fdcae5f0a36b4b9975214dea332e91a0
Jason Zimdars <jz@37signals.com>
2025-08-20 17:30:21 -0500

    Show 3 cols in public view

--- app/controllers/public/collections_controller.rb
+++ app/controllers/public/collections_controller.rb
@@ -7,6 +7,7 @@ class Public::CollectionsController < ApplicationController
 
   def show
     @considering = current_page_from @collection.cards.considering.latest, per_page: CardsController::PAGE_SIZE
+    @on_deck = current_page_from @collection.cards.on_deck.latest, per_page: CardsController::PAGE_SIZE
     @doing = current_page_from @collection.cards.doing.latest, per_page: CardsController::PAGE_SIZE
     @closed = current_page_from @collection.cards.closed.recently_closed_first, per_page: CardsController::PAGE_SIZE
 
