80342173b32654dddba3c208ef6ed347305bfff6
Jorge Manrubia <jorge@37signals.com>
2025-08-26 16:59:01 +0200

    Make sure they don't overlap on creation

--- app/models/notification/bundle.rb
+++ app/models/notification/bundle.rb
@@ -4,7 +4,7 @@ class Notification::Bundle < ApplicationRecord
   enum :status, %i[ pending processing delivered ]
 
   scope :due, -> { pending.where("ends_at <= ?", Time.current) }
-  scope :containing, -> (notification) { where("starts_at <= ? AND ends_at >= ?", notification.created_at, notification.created_at) }
+  scope :containing, -> (notification) { where("starts_at <= ? AND ends_at > ?", notification.created_at, notification.created_at) }
   scope :overlapping_with, -> (other_bundle) {
     where(
       "(starts_at <= ? AND ends_at >= ?) OR (starts_at <= ? AND ends_at >= ?) OR (starts_at >= ? AND ends_at <= ?)",
