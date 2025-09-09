271321f98a9718b5b7fb9ae386abadc06faa90e9
Jorge Manrubia <jorge@37signals.com>
2025-09-09 12:39:28 +0200

    Add http caching to notifications

--- app/controllers/notifications/trays_controller.rb
+++ app/controllers/notifications/trays_controller.rb
@@ -1,5 +1,7 @@
 class Notifications::TraysController < ApplicationController
   def show
     @notifications = Current.user.notifications.unread.ordered.limit(100)
+
+    fresh_when @notifications
   end
 end
