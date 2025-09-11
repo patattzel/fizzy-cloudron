c2c4534129966e2d6dd412a30488f5b1165d0285
Mike Dalessio <mike@37signals.com>
2025-09-11 18:04:17 -0400

    Removing unnecessary test files

--- gems/fizzy-saas/test/dummy/config/routes.rb
+++
@@ -1,3 +0,0 @@
-Rails.application.routes.draw do
-  mount Fizzy::Saas::Engine => "/fizzy-saas"
-end
--- gems/fizzy-saas/test/fizzy/saas_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class Fizzy::SaasTest < ActiveSupport::TestCase
-  test "it has a version number" do
-    assert Fizzy::Saas::VERSION
-  end
-end
