7106d4eaf805c6831ec36237cb26dee689238268
Stanko K.R. <stanko@stanko.io>
2025-09-12 15:57:45 +0200

    Remove concurrency limit
    It won't be useful in practice for now, and we can always put it back in if the need arises

--- app/jobs/webhook/delivery_job.rb
+++ app/jobs/webhook/delivery_job.rb
@@ -1,6 +1,5 @@
 class Webhook::DeliveryJob < ApplicationJob
   queue_as :webhooks
-  limits_concurrency to: 1, key: ->(delivery) { delivery.webhook_id }
 
   def perform(delivery)
     delivery.deliver
