f1a28d91225bf08a13cc58408c211add2397740a
Andy Smith <andy@37signals.com>
2025-09-25 16:29:13 -0500

    Extract from drag and drop

--- app/javascript/controllers/drag_and_strum_controller.js
+++ app/javascript/controllers/drag_and_strum_controller.js
@@ -2,22 +2,22 @@ import { Controller } from "@hotwired/stimulus"
 import { post } from "@rails/request.js"
 import { nextFrame } from "helpers/timing_helpers"
 
+const MANDOLIN_NOTES = [
+  "/audio/mandolin/B3.mp3",
+  "/audio/mandolin/C3.mp3",
+  "/audio/mandolin/D4.mp3",
+  "/audio/mandolin/E3.mp3",
+  "/audio/mandolin/Fsharp4.mp3",
+  "/audio/mandolin/G3.mp3"
+]
+
 export default class extends Controller {
   static targets = [ "item", "container" ]
   static values = { url: String }
   static classes = [ "draggedItem", "hoverContainer" ]
 
   connect() {
-    this.audioFiles = [
-      "/audio/B3.mp3",
-      "/audio/C3.mp3",
-      "/audio/D4.mp3",
-      "/audio/E3.mp3",
-      "/audio/Fsharp4.mp3",
-      "/audio/G3.mp3"
-    ]
-
-    this.preloadedAudioFiles = this.audioFiles.map(file => {
+    this.preloadedAudioFiles = MANDOLIN_NOTES.map(file => {
       const audio = new Audio(file)
       audio.load()
       return audio
@@ -30,8 +30,8 @@ export default class extends Controller {
 
     if (!container) { return }
 
-    if (container !== this.sourceContainer) {
-      this.#playZither()
+    if (container !== this.sourceContainer && event.shiftKey) {
+      this.#playSound()
     }
   }
 
@@ -39,7 +39,7 @@ export default class extends Controller {
     return this.containerTargets.find(container => container.contains(element) || container === element)
   }
 
-  #playZither() {
+  #playSound() {
     const randomIndex = Math.floor(Math.random() * this.preloadedAudioFiles.length)
     const audio = this.preloadedAudioFiles[randomIndex]
     const audioInstance = new Audio(audio.src)
