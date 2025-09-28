9579a4b87921e16e83549e416a894252c2edf47d
Jorge Manrubia <jorge@hey.com>
2025-09-28 11:06:40 +0200

    Rename columns

--- app/controllers/collections/columns_controller.rb
+++ app/controllers/collections/columns_controller.rb
@@ -1,11 +1,16 @@
 class Collections::ColumnsController < ApplicationController
-  include CollectionScoped
+  include ActionView::RecordIdentifier, CollectionScoped
 
-  before_action :set_column, only: :show
+  before_action :set_column, only: [:show, :update]
 
   def create
     @column = @collection.columns.create!(column_params)
-    render turbo_stream: turbo_stream.before("closed-cards", partial: "collections/show/column", locals: { column: @column })
+    render turbo_stream: turbo_stream.before("closed-cards", partial: "collections/show/column", method: :morph, locals: { column: @column })
+  end
+
+  def update
+    @column.update!(column_params)
+    render turbo_stream: turbo_stream.replace(dom_id(@column), partial: "collections/show/column", method: :morph, locals: { column: @column })
   end
 
   def show
