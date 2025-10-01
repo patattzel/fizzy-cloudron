ea1336fdd7631a093310d803c77682966c0f1685
Andy Smith <andy@37signals.com>
2025-10-01 16:56:55 -0500

    Use controller to disable empty comment submit

--- app/javascript/controllers/form_controller.js
+++ app/javascript/controllers/form_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 import { debounce } from "helpers/timing_helpers";
 
 export default class extends Controller {
-  static targets = [ "cancel" ]
+  static targets = [ "cancel", "submit" ]
 
   static values = {
     debounceTimeout: { type: Number, default: 300 }
@@ -35,6 +35,16 @@ export default class extends Controller {
 
   preventAttachment(event) {
     event.preventDefault()
+  }
+
+  disableEmptySubmit(event) {
+    var value = event.target.value.replace(/<\/?[^>]+(>|$)/g, "")
+
+    if (value) {
+      this.submitTarget.removeAttribute("disabled")
+    } else {
+      this.submitTarget.setAttribute("disabled", true)
+    }
   }
 
   select(event) {
