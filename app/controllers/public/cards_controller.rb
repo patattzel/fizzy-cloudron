a7fd4018d6a5953b04b0099b1b28d99efbcb84c1
Kevin McConnell <kevin@37signals.com>
2025-10-23 11:44:26 -0700

    Extend public caching to 30 seconds
    Up from the previous 5 seconds. It's still short enough that the pages
    shouldn't feel stale, but it further reduces the number of requests that
    will reach the app.
    
    Also moved this into a shared concern so we can adjust the caching rule
    in one place.

--- app/controllers/public/cards_controller.rb
+++ app/controllers/public/cards_controller.rb
@@ -1,12 +1,10 @@
 class Public::CardsController < ApplicationController
-  include PublicCardScoped
+  include CachedPublicly, PublicCardScoped
 
   allow_unauthenticated_access only: :show
 
   layout "public"
 
   def show
-    # To enable caching at intermediate proxies during traffic spikes
-    expires_in 5.seconds, public: true
   end
 end
--- app/controllers/public/collections/columns/not_nows_controller.rb
+++ app/controllers/public/collections/columns/not_nows_controller.rb
@@ -1,5 +1,5 @@
 class Public::Collections::Columns::NotNowsController < ApplicationController
-  include PublicCollectionScoped
+  include CachedPublicly, PublicCollectionScoped
 
   allow_unauthenticated_access only: :show
 
@@ -7,8 +7,5 @@ class Public::Collections::Columns::NotNowsController < ApplicationController
 
   def show
     set_page_and_extract_portion_from @collection.cards.postponed.reverse_chronologically.with_golden_first
-
-    # To enable caching at intermediate proxies during traffic spikes
-    expires_in 5.seconds, public: true
   end
 end
