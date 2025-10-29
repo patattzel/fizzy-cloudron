ae33109e5e3bdf69f02d417e440f7411411786c6
Fernando Álvarez <fernando@37signals.com>
2025-10-29 10:06:33 +0100

    Amend references to box-car.com

--- script/configure-lb-production.sh
+++ script/configure-lb-production.sh
@@ -27,21 +27,21 @@ ssh app@fizzy-lb-101.df-iad-int.37signals.com \
 
 # fizzy-lb-01.sc-chi-int.37signals.com
 #
-# Service      Host                                                                                                                                                                                                                   Path    Target                                                                        State    TLS
-# fizzy        37s.fizzy.37signals.com,fizzy.37signals.com,dev.fizzy.37signals.com,fizzy.37signals.com,qa.fizzy.37signals.com,fizzy.37signals.com,37s.box-car.com,box-car.com,dev.box-car.com,box-car.com,qa.box-car.com,box-car.com  /       fizzy-app-101.df-iad-int.37signals.com,fizzy-app-02.sc-chi-int.37signals.com  running  yes
-# fizzy-admin  fizzy.37signals.com,box-car.com                                                                                                                                                                                        /admin  fizzy-app-101.df-iad-int.37signals.com                                        running  yes                                                                                                                                                                                                 /admin  fizzy-app-101.df-iad-int.37signals.com                                        running  yes
+#   Service       Host                             Path    Target                                                                        State    TLS
+#   boxcar        fizzy.37signals.com,box-car.com  /       fizzy-app-101.df-iad-int.37signals.com,fizzy-app-02.sc-chi-int.37signals.com  running  yes
+#   boxcar-admin  fizzy.37signals.com,box-car.com  /admin  fizzy-app-101.df-iad-int.37signals.com                                        running  yes
 ssh app@fizzy-lb-01.sc-chi-int.37signals.com \
   docker exec fizzy-load-balancer \
-    kamal-proxy deploy fizzy \
+    kamal-proxy deploy boxcar \
       --tls \
-      --host={37s,dev,qa}.fizzy.37signals.com,fizzy.37signals.com,{37s,dev,qa}.box-car.com,box-car.com \
+      --host=fizzy.37signals.com,box-car.com \
       --target=fizzy-app-101.df-iad-int.37signals.com \
       --read-target=fizzy-app-02.sc-chi-int.37signals.com \
       --tls-acme-cache-path=/certificates
 
 ssh app@fizzy-lb-01.sc-chi-int.37signals.com \
   docker exec fizzy-load-balancer \
-    kamal-proxy deploy fizzy-admin \
+    kamal-proxy deploy boxcar-admin \
       --host=fizzy.37signals.com,box-car.com \
       --path-prefix /admin \
       --strip-path-prefix=false \
