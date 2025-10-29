ec5833cdd8bbbdf1c3fe15082dd9c3ebade1094e
David Heinemeier Hansson <david@hey.com>
2025-10-29 18:11:43 +0100

    Extract explaining helper method

--- app/helpers/collections_helper.rb
+++ app/helpers/collections_helper.rb
@@ -1,4 +1,10 @@
 module CollectionsHelper
+  def hotkeyed_link_to_collection(collection, &)
+    link_to collection, class: "header__title btn borderless txt-large",
+      style: "--btn-padding: 0.25ch 1ch 0.25ch 0.75ch; view-transistion-name: card-collection-title;",
+      data: { controller: "hotkey", action: "keydown.esc@document->hotkey#click click->turbo-navigation#backIfSamePath" }, &
+  end
+
   def referenced_collections_from(records)
     Current.user.collections.where id: records.pluck(:collection_id).uniq
   end
