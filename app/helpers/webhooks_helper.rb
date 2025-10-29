4f6e27e2139a161e222ff21f824105492d6908fd
Jason Zimdars <jz@37signals.com>
2025-10-29 11:11:04 -0500

    Display actions in logical order

--- app/helpers/webhooks_helper.rb
+++ app/helpers/webhooks_helper.rb
@@ -1,23 +1,21 @@
 module WebhooksHelper
   ACTION_LABELS = {
+    card_published: "Card added",
+    card_title_changed: "Card title changed",
+    card_collection_changed: "Card collection changed",
+    comment_created: "Comment added",
     card_assigned: "Card assigned",
+    card_unassigned: "Card unassigned",
+    card_triaged: "Card column changed",
     card_closed: "Card moved to “Done”",
+    card_reopened: "Card reopened",
     card_postponed: "Card moved to “Not Now”",
     card_auto_postponed: "Card auto-closed as “Not Now”",
-    card_collection_changed: "Card collection changed",
-    card_published: "Card added",
-    card_reopened: "Card reopened",
-    card_sent_back_to_triage: "Card move back to “Maybe?”",
-    card_title_changed: "Card title changed",
-    card_triaged: "Card column changed",
-    card_unassigned: "Card unassigned",
-    comment_created: "Comment added"
+    card_sent_back_to_triage: "Card moved back to “Maybe?”"
   }.with_indifferent_access.freeze
 
   def webhook_action_options(actions = Webhook::PERMITTED_ACTIONS)
-    actions.each_with_object({}) do |action, hash|
-      hash[action.to_s] = webhook_action_label(action)
-    end
+    ACTION_LABELS.select { |key, _| actions.include?(key.to_s) }
   end
 
   def webhook_action_label(action)
