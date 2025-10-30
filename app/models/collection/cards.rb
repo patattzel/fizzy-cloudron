4da2313e889a67095510052f1f0afc3c324c6972
Jorge Manrubia <jorge@hey.com>
2025-10-30 16:42:28 +0100

    This method should be private

--- app/models/collection/cards.rb
+++ app/models/collection/cards.rb
@@ -7,7 +7,8 @@ module Collection::Cards
     after_update_commit :touch_all_cards_later, if: :saved_change_to_name?
   end
 
-  def touch_all_cards_later
-    Card::TouchAllJob.perform_later(self)
-  end
+  private
+    def touch_all_cards_later
+      Card::TouchAllJob.perform_later(self)
+    end
 end
