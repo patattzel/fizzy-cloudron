4d4eb276817b87ea80a437f14dec772010182692
Jorge Manrubia <jorge@hey.com>
2025-10-30 14:52:30 +0100

    Add http caching for the new columns/edit card resource

--- app/controllers/cards/columns_controller.rb
+++ app/controllers/cards/columns_controller.rb
@@ -1,5 +1,8 @@
 class Cards::ColumnsController < ApplicationController
   def edit
     @card = Current.user.accessible_cards.find(params[:card_id])
+    @columns = @card.collection.columns
+
+    fresh_when etag: [ @card, @columns ]
   end
 end
