afc53d3f59b528dea6fbd05c6be235f0ecafa422
David Heinemeier Hansson <david@hey.com>
2025-10-30 16:03:10 +0100

    Extract helper instead of using anemic partial

--- app/helpers/collections_helper.rb
+++ app/helpers/collections_helper.rb
@@ -7,6 +7,12 @@ module CollectionsHelper
     end
   end
 
+  def link_to_edit_collection(collection)
+    link_to edit_collection_path(collection), class: "btn", data: { controller: "tooltip" } do
+      icon_tag("settings") + tag.span("Settings for #{collection.name}", class: "for-screen-reader")
+    end
+  end
+
   def referenced_collections_from(records)
     Current.user.collections.where id: records.pluck(:collection_id).uniq
   end
