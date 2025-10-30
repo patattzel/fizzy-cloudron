df61bf086949580def6e24ea52091bfcc2b4db1d
David Heinemeier Hansson <david@hey.com>
2025-10-30 15:34:42 +0100

    Extract helper at a higher level

--- app/helpers/collections_helper.rb
+++ app/helpers/collections_helper.rb
@@ -1,8 +1,10 @@
 module CollectionsHelper
-  def hotkeyed_link_to_collection(collection, &)
+  def link_back_to_collection(collection)
     link_to collection, class: "header__title btn borderless txt-large",
       style: "--btn-padding: 0.25ch 1ch 0.25ch 0.75ch; view-transistion-name: card-collection-title;",
-      data: { controller: "hotkey", action: "keydown.esc@document->hotkey#click click->turbo-navigation#backIfSamePath" }, &
+      data: { controller: "hotkey", action: "keydown.esc@document->hotkey#click click->turbo-navigation#backIfSamePath" } do
+        tag.span ("&larr;" + tag.strong(collection.name, class: "font-black")).html_safe
+    end
   end
 
   def referenced_collections_from(records)
