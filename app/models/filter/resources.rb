e62e837e62a6c9264638f7a63f9f2582dd71e0df
Jason Zimdars <jz@37signals.com>
2025-10-31 11:18:25 -0500

    Fussy capitalization for shared text

--- app/models/filter/resources.rb
+++ app/models/filter/resources.rb
@@ -23,7 +23,7 @@ module Filter::Resources
 
   def collection_titles
     if collections.none?
-      Collection.one? ? [ Collection.first.name ] : [ "All boards" ]
+      Collection.one? ? [ Collection.first.name ] : [ "all boards" ]
     else
       collections.map(&:name)
     end
