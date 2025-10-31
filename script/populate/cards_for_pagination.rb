384d0259b3b2ced2412ffd8785a61d6c3a865087
David Heinemeier Hansson <david@hey.com>
2025-10-31 15:47:53 +0100

    Fix-up population script and take it to 10K cards

--- script/populate/cards_for_pagination.rb
+++ script/populate/cards_for_pagination.rb
@@ -1,30 +1,15 @@
 require_relative "../../config/environment"
 
-CARDS_COUNT = 200
+CARDS_COUNT = 10_000
 
-ApplicationRecord.current_tenant = "37signals"
+ApplicationRecord.current_tenant = "897362094"
 account = Account.sole
 user = User.first
+user.sessions.create!
 Current.session = user.sessions.last
 collection = Collection.first
 
-# Doing
-
 CARDS_COUNT.times do |index|
   card = collection.cards.create!(title: "Doing card #{index}", creator: user, status: :published)
-  card.engage
-end
-
-# Considering
-
-CARDS_COUNT.times do |index|
-  card = collection.cards.create!(title: "Considering card #{index}", creator: user, status: :published)
-  card.reconsider
-end
-
-# Completed
-
-CARDS_COUNT.times do |index|
-  card = collection.cards.create!(title: "Popped card #{index}", creator: user, status: :published)
-  card.close
+  puts "."
 end
