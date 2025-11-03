36c0ce0f6536b773a606ce35eb9e672287f77b62
David Heinemeier Hansson <david@hey.com>
2025-11-03 16:00:35 +0100

    Split callbacks for easier overwriting

--- app/controllers/public/collections/columns_controller.rb
+++ app/controllers/public/collections/columns_controller.rb
@@ -1,14 +1,13 @@
 class Public::Collections::ColumnsController < Public::BaseController
-  before_action :set_column, only: :show
+  before_action :set_column
 
   def show
     set_page_and_extract_portion_from @column.cards.active.latest.with_golden_first
   end
 
   private
-    # Unlike the other public controllers, this is using params[:id] to fetch the column
-    def set_collection_and_card
-      @collection = Collection.find_by_published_key(params[:collection_id])
+    # Unlike the other public controllers, this is using params[:id] to fetch the column instead of the card
+    def set_card
     end
 
     def set_column
