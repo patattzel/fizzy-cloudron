59fb382d666d992bafc674c05c8b10061974190b
Jorge Manrubia <jorge@hey.com>
2025-11-06 11:05:42 +0100

    Add some sanity tests for event descriptions

---
+++ test/models/event/description_test.rb
@@ -0,0 +1,35 @@
+require "test_helper"
+
+class Event::DescriptionTest < ActiveSupport::TestCase
+  test "generates html description for card published event" do
+    description = events(:logo_published).description_for(users(:david))
+
+    assert_includes description.to_html, "added"
+    assert_includes description.to_html, "logo"
+  end
+
+  test "generates plain text description for card published event" do
+    description = events(:logo_published).description_for(users(:david))
+
+    assert_includes description.to_plain_text, "You added"
+    assert_includes description.to_plain_text, "logo"
+  end
+
+  test "generates description for comment event" do
+    description = events(:layout_commented).description_for(users(:jz))
+
+    assert_includes description.to_plain_text, "David commented on"
+  end
+
+  test "uses 'You' when event creator is the current user" do
+    description = events(:logo_published).description_for(users(:david))
+
+    assert_includes description.to_plain_text, "You added"
+  end
+
+  test "uses creator name when event creator is not the current user" do
+    description = events(:logo_published).description_for(users(:jz))
+
+    assert_includes description.to_plain_text, "David added"
+  end
+end
