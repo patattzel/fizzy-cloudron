f94f84139326e4d8e72b9916f173ef02bec2b636
Kevin McConnell <kevin@37signals.com>
2025-11-06 09:56:28 +0000

    Serve default error pages

--- config/application.rb
+++ config/application.rb
@@ -27,9 +27,6 @@ module Fizzy
     # enable load_async
     config.active_record.async_query_executor = :global_thread_pool
 
-    # use routes for error pages instead of static files
-    config.exceptions_app = self.routes
-
     # include the tenant in query logs
     config.active_record.query_log_tags_enabled = true
     config.active_record.query_log_tags = [ :tenant ]
