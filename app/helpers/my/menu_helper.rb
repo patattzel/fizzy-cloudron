7d74f1c0c5e886cd84f89d318b13a3a78e52311b
David Heinemeier Hansson <david@hey.com>
2025-11-08 17:35:07 +0100

    No need for partials here either

--- app/helpers/my/menu_helper.rb
+++ app/helpers/my/menu_helper.rb
@@ -16,4 +16,37 @@ module My::MenuHelper
         navigable_list_target: "input",
         action: "input->filter#filter" }
   end
+
+  def my_menu_board_item(board)
+    tag.li(class: "popup__item", data: { filter_target: "item", navigable_list_target: "item", id: "filter-board-#{board.id}" }) do
+      icon_tag("board", class: "popup__icon") +
+      link_to(tag.span(board.name, class: "overflow-ellipsis"), board, class: "popup__btn btn")
+    end
+  end
+
+  def my_menu_tag_item(the_tag)
+    tag.li(class: "popup__item", data: { filter_target: "item", navigable_list_target: "item", id: "filter-tag-#{the_tag.id}" }) do
+      icon_tag("tag", class: "popup__icon") +
+      link_to(tag.span("#{the_tag.title} (#{the_tag.cards_count})", class: "overflow-ellipsis"), cards_path(tag_ids: [the_tag]), class: "popup__btn btn")
+    end
+  end
+
+  def my_menu_user_item(user)
+    tag.li(class: "popup__item", data: { filter_target: "item", navigable_list_target: "item", id: "filter-user-#{user.id}" }) do
+      icon_tag("person", class: "popup__icon") +
+      link_to(tag.span(user.name, class: "overflow-ellipsis"), user, class: "popup__btn btn")
+    end
+  end
+
+  def my_menu_filter_item(filter)
+    tag.li(class: "popup__item", data: { filter_target: "item", navigable_list_target: "item", id: "filter-custom-#{filter.id}" }) do
+      icon_tag("bookmark", class: "popup__icon") +
+      link_to(cards_path(filter_id: filter.id), class: "popup__btn btn") do
+        tag.div(class: "txt-tight-lines min-width txt-small overflow-ellipsis") do
+          tag.div(tag.strong(filter.boards_label)) +
+          tag.div(filter.summary, class: "txt-capitalize")
+        end
+      end
+    end
+  end
 end
