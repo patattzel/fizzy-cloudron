7ea933c4578fdd2076aa39c735645e038db813ef
Stanko K.R. <stanko@stanko.io>
2025-11-10 19:54:50 +0100

    Add account_id to everything

--- app/models/column.rb
+++ app/models/column.rb
@@ -1,6 +1,7 @@
 class Column < ApplicationRecord
   include Colored, Positioned
 
+  belongs_to :account, default: -> { Current.account }
   belongs_to :board, touch: true
   has_many :cards, dependent: :nullify
 
