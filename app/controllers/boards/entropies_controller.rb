4d26a7217e07b5eaa5ea07fe5618ec41973555b5
Jorge Manrubia <jorge@hey.com>
2025-11-12 11:45:09 +0100

    Ensure only admins/creators can touch the board entropy

--- app/controllers/boards/entropies_controller.rb
+++ app/controllers/boards/entropies_controller.rb
@@ -1,11 +1,19 @@
 class Boards::EntropiesController < ApplicationController
   include BoardScoped
 
+  before_action :ensure_permission_to_admin_board
+
   def update
     @board.entropy.update!(entropy_params)
   end
 
   private
+    def ensure_permission_to_admin_board
+      unless Current.user.can_administer_board?(@board)
+        head :forbidden
+      end
+    end
+
     def entropy_params
       params.expect(board: [ :auto_postpone_period ])
     end
