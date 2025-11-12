8715f98e76ed9566acf01b69a5dea2def89d1d6c
Donal McBreen <donal@37signals.com>
2025-11-12 17:39:30 +0000

    Set up connects_to before id attribute

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,7 +1,7 @@
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
 
-  attribute :id, :uuid, default: -> { ActiveRecord::Type::Uuid.generate }
-
   connects_to database: { writing: :primary, reading: :replica }
+
+  attribute :id, :uuid, default: -> { ActiveRecord::Type::Uuid.generate }
 end
