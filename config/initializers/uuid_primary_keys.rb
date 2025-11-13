5de9858a372b09dba822d721b12f808feb6e5b96
Donal McBreen <donal@37signals.com>
2025-11-13 13:04:14 +0000

    Use uuid as the id type in schema.rb
    This matches the format in the migration files and makes the type_to_sql
    override redundant.
    
    The only wart we are left with is that since there are no true UUID
    types in MySQL we'll have to assume that binary(16) columns are UUIDs.
    
    In practice this is probably fine as the MySQL adapter doesn't map any
    other types to binary.

--- config/initializers/uuid_primary_keys.rb
+++ config/initializers/uuid_primary_keys.rb
@@ -6,15 +6,6 @@ ActiveSupport.on_load(:active_record) do
       @native_database_types_with_uuid ||= super.merge(uuid: { name: "binary", limit: 16 })
     end
 
-    # Override type_to_sql to use binary instead of varbinary for UUID columns
-    def type_to_sql(type, limit: nil, **options)
-      if type.to_s == "binary" && limit == 16
-        "binary(16)"
-      else
-        super
-      end
-    end
-
     # Override lookup_cast_type to recognize binary(16) as UUID type
     def lookup_cast_type(sql_type)
       if sql_type == "binary(16)"
@@ -27,18 +18,18 @@ ActiveSupport.on_load(:active_record) do
 
   ActiveRecord::ConnectionAdapters::AbstractMysqlAdapter.prepend(MysqlUuidAdapter)
 
-  module SchemaDumperBinaryLimit
-    def prepare_column_options(column)
-      spec = super
-      # Ensure binary columns with limits always include them in schema
-      if column.type == :binary && column.sql_type =~ /binary\((\d+)\)/
-        spec[:limit] = $1.to_i
+  module SchemaDumperUuidType
+    # Map binary(16) columns to :uuid type in schema.rb
+    def schema_type(column)
+      if column.sql_type == "binary(16)"
+        :uuid
+      else
+        super
       end
-      spec
     end
   end
 
-  ActiveRecord::ConnectionAdapters::MySQL::SchemaDumper.prepend(SchemaDumperBinaryLimit)
+  ActiveRecord::ConnectionAdapters::MySQL::SchemaDumper.prepend(SchemaDumperUuidType)
 
   module TableDefinitionUuidSupport
     def uuid(name, **options)
