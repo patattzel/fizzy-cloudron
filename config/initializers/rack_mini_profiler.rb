47bd4654c27b29a45f53f2445af6951012e29c54
Mike Dalessio <mike@37signals.com>
2025-11-17 14:54:42 -0500

    rubocop: more style fixes

--- config/initializers/rack_mini_profiler.rb
+++ config/initializers/rack_mini_profiler.rb
@@ -2,6 +2,6 @@ if defined?(Rack::MiniProfiler)
   Rack::MiniProfiler.config.tap do |config|
     config.position = "top-right"
     config.enable_hotwire_turbo_drive_support = true
-    config.pre_authorize_cb = -> (_env) { File.exist?(Rails.root.join("tmp/rack-mini-profiler-dev.txt")) }
+    config.pre_authorize_cb = ->(_env) { File.exist?(Rails.root.join("tmp/rack-mini-profiler-dev.txt")) }
   end
 end
