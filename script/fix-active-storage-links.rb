6eaedb4d1e3971e4bc51859b8d47203cc657d8ac
Stanko K.R. <stanko@stanko.io>
2025-11-18 19:39:32 +0100

    Decode the filename form the URL

--- script/fix-active-storage-links.rb
+++ script/fix-active-storage-links.rb
@@ -11,13 +11,22 @@ ActionText::RichText.all.where("body LIKE '%/rails/active_storage/%'").find_each
   blobs = rich_text.embeds.map(&:blob)
 
   rich_text.body.send(:attachment_nodes).each do |node|
-    filename = node["filename"]
+    url = node["url"]
+    next unless url
 
+    url_encoded_filename = url.split("/").last
+    filename = URI.decode_www_form_component(url_encoded_filename)
+
+    counter += 1
     blob = blobs.select { |b| b.filename == filename }
     raise "Multiple blobs with filename #{filename}" if blob.size > 1
     blob = blob.first
 
-    node["sgid"] = blob.attachable_sgid
+    if blob
+      node["sgid"] = blob.attachable_sgid
+    else
+      skipped += 1
+    end
   end
 
   rich_text.save!
