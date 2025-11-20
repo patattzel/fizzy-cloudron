41f2638b832cc3837d704df96bf2658bbe46754d
Stanko K.R. <stanko@stanko.io>
2025-11-20 10:49:29 +0100

    Fix links in previews

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -3,6 +3,7 @@ class UserMailerPreview < ActionMailer::Preview
     new_email_address = "new.email@example.com"
     user = User.active.sample
     token = user.send(:generate_email_address_change_token, to: new_email_address)
+    Current.account = user.account
 
     UserMailer.email_change_confirmation(
       email_address: new_email_address,
