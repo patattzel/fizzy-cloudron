08091d93c8a0514a20be333f56e59e2955719931
Donal McBreen <donal@37signals.com>
2025-11-21 12:37:29 +0000

    Drop parentheses

--- app/models/search/record.rb
+++ app/models/search/record.rb
@@ -31,7 +31,7 @@ class Search::Record < ApplicationRecord
     for_query(query: query, user: user)
       .includes(:searchable, card: [ :board, :creator ])
       .order(created_at: :desc)
-      .select(:id, :account_id, :searchable_type, :searchable_id, :card_id, :board_id, :title, :content, :created_at, *(search_fields(query)))
+      .select(:id, :account_id, :searchable_type, :searchable_id, :card_id, :board_id, :title, :content, :created_at, *search_fields(query))
   end
 
   def source
