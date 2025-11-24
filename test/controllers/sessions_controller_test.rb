48641fa78620f0c98684df48c251bcefa40546aa
Jorge Manrubia <jorge@hey.com>
2025-11-24 15:43:16 +0100

    Test moved to the gem

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -21,22 +21,6 @@ class SessionsControllerTest < ActionDispatch::IntegrationTest
     end
   end
 
-  if Fizzy.saas?
-    test "create for a new user" do
-      untenanted do
-        assert_difference -> { Identity.count }, +1 do
-          assert_difference -> { MagicLink.count }, +1 do
-            post session_path,
-              params: { email_address: "nonexistent-#{SecureRandom.hex(6)}@example.com" },
-              headers: http_basic_auth_headers("testname", "testpassword")
-          end
-        end
-
-        assert_redirected_to session_magic_link_path
-      end
-    end
-  end
-
   test "destroy" do
     sign_in_as :kevin
 
@@ -47,9 +31,4 @@ class SessionsControllerTest < ActionDispatch::IntegrationTest
       assert_not cookies[:session_token].present?
     end
   end
-
-  private
-    def http_basic_auth_headers(user, password)
-      { "Authorization" => ActionController::HttpAuthentication::Basic.encode_credentials(user, password) }
-    end
 end
