c4f726862fc883b3a8ad44fcbbdf910b51cd18e6
Jorge Manrubia <jorge@hey.com>
2025-11-29 21:45:48 +0100

    Move solid-queue dev setup to the engine

--- saas/config/environments/development.rb
+++ saas/config/environments/development.rb
@@ -2,4 +2,9 @@ Rails.application.configure do
   if Rails.root.join("tmp/structured-logging.txt").exist?
     config.structured_logging.logger = ActiveSupport::Logger.new("log/structured-development.log")
   end
+
+  if Rails.root.join("tmp/solid-queue.txt").exist?
+    config.active_job.queue_adapter = :solid_queue
+    config.solid_queue.connects_to = { database: { writing: :queue, reading: :queue } }
+  end
 end
