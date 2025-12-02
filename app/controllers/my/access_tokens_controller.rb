1f2f1e0b4b94b5d059730f6a602ec53da855e142
David Heinemeier Hansson <david@hey.com>
2025-12-02 15:03:17 +0100

    Only allow new token to be viewed within 10 seconds

--- app/controllers/my/access_tokens_controller.rb
+++ app/controllers/my/access_tokens_controller.rb
@@ -3,13 +3,22 @@ class My::AccessTokensController < ApplicationController
     @access_tokens = Current.identity.access_tokens.order(created_at: :desc)
   end
 
+  def show
+    access_token_id = verifier.verify(params[:id])
+    @access_token = Current.identity.access_tokens.find(access_token_id)
+  rescue ActiveSupport::MessageVerifier::InvalidSignature
+    redirect_to my_access_tokens_path, alert: "Token is no longer visible"
+  end
+
   def new
     @access_token = Current.identity.access_tokens.new
   end
 
   def create
-    @access_token = Current.identity.access_tokens.create!(access_token_params)
-    redirect_to my_access_tokens_path
+    access_token = Current.identity.access_tokens.create!(access_token_params)
+    expiring_id = verifier.generate access_token.id, expires_in: 10.seconds
+
+    redirect_to my_access_token_path(expiring_id)
   end
 
   def destroy
@@ -20,5 +29,9 @@ class My::AccessTokensController < ApplicationController
   private
     def access_token_params
       params.expect(access_token: [ :description, :permission ])
+    end
+
+    def verifier
+      Rails.application.message_verifier(:access_tokens)
     end
 end
