83b53a7ec5bb5322a6d2c1a27c986282617bf621
Stanko K.R. <stanko@stanko.io>
2025-12-11 19:17:39 +0100

    Prohibit access to magic links unless an email address

--- app/controllers/sessions/magic_links_controller.rb
+++ app/controllers/sessions/magic_links_controller.rb
@@ -2,6 +2,7 @@ class Sessions::MagicLinksController < ApplicationController
   disallow_account_scope
   require_unauthenticated_access
   rate_limit to: 10, within: 15.minutes, only: :create, with: -> { redirect_to session_magic_link_path, alert: "Wait 15 minutes, then try again" }
+  before_action :ensure_that_email_address_pending_authentication_exists
 
   layout "public"
 
@@ -17,6 +18,12 @@ class Sessions::MagicLinksController < ApplicationController
   end
 
   private
+    def ensure_that_email_address_pending_authentication_exists
+      unless email_address_pending_authentication.present?
+        redirect_to new_session_path, alert: "Enter your email address to sign in."
+      end
+    end
+
     def authenticate_with(magic_link)
       if email_address_pending_authentication_matches?(magic_link.identity.email_address)
         start_new_session_for magic_link.identity
