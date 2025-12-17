384300b735f94706a174bc62501601ac3ab1e832
Jorge Manrubia <jorge@hey.com>
2025-12-17 08:44:11 +0100

    Add additional plan for extra storage

--- saas/app/models/plan.rb
+++ saas/app/models/plan.rb
@@ -1,7 +1,8 @@
 class Plan
   PLANS = {
     free_v1: { name: "Free", price: 0, card_limit: 1000, storage_limit: 1.gigabytes },
-    monthly_v1: { name: "Unlimited", price: 20, card_limit: Float::INFINITY, storage_limit: 5.gigabytes, stripe_price_id: ENV["STRIPE_MONTHLY_V1_PRICE_ID"] }
+    monthly_v1: { name: "Unlimited", price: 20, card_limit: Float::INFINITY, storage_limit: 5.gigabytes, stripe_price_id: ENV["STRIPE_MONTHLY_V1_PRICE_ID"] },
+    monthly_extra_storage_v1: { name: "Unlimited with extra storage", price: 25, card_limit: Float::INFINITY, storage_limit: 500.gigabytes, stripe_price_id: ENV["STRIPE_MONTHLY_EXTRA_STORAGE_V1_PRICE_ID"] }
   }
 
   attr_reader :key, :name, :price, :card_limit, :storage_limit, :stripe_price_id
@@ -17,6 +18,10 @@ class Plan
 
     def paid
       @paid ||= find(:monthly_v1)
+    end
+
+    def paid_with_extra_storage
+      @paid_with_extra_storage ||= find(:monthly_extra_storage_v1)
     end
 
     def find(key)
