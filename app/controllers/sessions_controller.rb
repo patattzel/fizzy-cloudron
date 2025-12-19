H4-Dev/fizzy

Allow JSON requests to send a magic link

app/controllers/sessions_controller.rb
@@ -1,7 +1,7 @@
 class SessionsController < ApplicationController
   disallow_account_scope
   require_unauthenticated_access except: :destroy
-  rate_limit to: 10, within: 3.minutes, only: :create, with: -> { redirect_to new_session_path, alert: "Try again later." }
+  rate_limit to: 10, within: 3.minutes, only: :create, with:  :rate_limit_exceeded
 
   layout "public"
 
@@ -9,12 +9,16 @@ class SessionsController < ApplicationController
   end
 
   def create
+
     if identity = Identity.find_by_email_address(email_address)
       magic_link = identity.send_magic_link
       serve_development_magic_link(magic_link)
     end
 
-    redirect_to session_magic_link_path
+    respond_to do |format|
+      format.html { redirect_to session_magic_link_path }
+      format.json { head :created  }
+    end
   end
 
   def destroy
@@ -25,5 +29,13 @@ class SessionsController < ApplicationController
   private
     def email_address
       params.expect(:email_address)
+    end
+
+    def rate_limit_exceeded
+      rate_limit_exceeded_message = "Try again later."
+      respond_to do |format|
+        format.html { redirect_to new_session_path, alert: rate_limit_exceeded_message }
+        format.json { render json: { message: rate_limit_exceeded_message }, status: :too_many_requests }
+      end
     end
 end

patattzel/fizzy-cloudron

app/controllers/sessions_controller.rb
@@ -1,7 +1,7 @@
 class SessionsController < ApplicationController
   disallow_account_scope
   require_unauthenticated_access except: :destroy
-  rate_limit to: 10, within: 3.minutes, only: :create, with: -> { redirect_to new_session_path, alert: "Try again later." }
+  rate_limit to: 10, within: 3.minutes, only: :create, with:  :rate_limit_exceeded
 
   layout "public"
 
@@ -10,15 +10,20 @@ class SessionsController < ApplicationController
 
   def create
     if identity = Identity.find_by_email_address(email_address)
-      redirect_to_session_magic_link identity.send_magic_link
+      magic_link = identity.send_magic_link
     else
       signup = Signup.new(email_address: email_address)
       if signup.valid?(:identity_creation)
         magic_link = signup.create_identity if Account.accepting_signups?
-        redirect_to_session_magic_link magic_link
       else
         head :unprocessable_entity
+        return
       end
+    end
+
+    respond_to do |format|
+      format.html { redirect_to_session_magic_link magic_link }
+      format.json { head :created }
     end
   end
 
@@ -30,5 +35,13 @@ class SessionsController < ApplicationController
   private
     def email_address
       params.expect(:email_address)
+    end
+
+    def rate_limit_exceeded
+      rate_limit_exceeded_message = "Try again later."
+      respond_to do |format|
+        format.html { redirect_to new_session_path, alert: rate_limit_exceeded_message }
+        format.json { render json: { message: rate_limit_exceeded_message }, status: :too_many_requests }
+      end
     end
 end