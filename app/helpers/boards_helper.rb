b69a40d0fb9fcfbb3d751e5528a06d47ada8ae35
Adrien Maston <adrien@37signals.com>
2026-01-08 17:32:21 +0100

    Use already existing buttons to populate the top nav bar

--- app/helpers/boards_helper.rb
+++ app/helpers/boards_helper.rb
@@ -5,7 +5,7 @@ module BoardsHelper
 
   def link_to_edit_board(board)
     link_to edit_board_path(board), class: "btn", data: {
-      controller: "tooltip"
+      controller: "tooltip", bridge__overflow_menu_target: "item", bridge_title: "Settings for #{board.name}"
     } do
       icon_tag("settings") + tag.span("Settings for #{board.name}", class: "for-screen-reader")
     end
