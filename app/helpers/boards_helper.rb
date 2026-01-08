809c1e2490c7048506486c5c042e8708e09a3561
Denis Švara <denis.svara@gmail.com>
2026-01-08 16:36:25 +0100

    Remove bridge button metadata from board settings link.

--- app/helpers/boards_helper.rb
+++ app/helpers/boards_helper.rb
@@ -5,10 +5,7 @@ module BoardsHelper
 
   def link_to_edit_board(board)
     link_to edit_board_path(board), class: "btn", data: {
-      controller: "tooltip",
-      bridge__buttons_target: "button",
-      bridge_icon_name: "gearshape.fill",
-      bridge_title: "Settings"
+      controller: "tooltip"
     } do
       icon_tag("settings") + tag.span("Settings for #{board.name}", class: "for-screen-reader")
     end
