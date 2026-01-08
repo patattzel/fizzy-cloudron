72e5f28bfc0c50e8a5cd7b0726fdf4b1665172e1
Denis Švara <denis.svara@gmail.com>
2026-01-08 14:29:33 +0100

    Update overflow menu controller to send an array of menu items.

--- app/javascript/bridge/controllers/bridge/overflow_menu_controller.js
+++ app/javascript/bridge/controllers/bridge/overflow_menu_controller.js
@@ -1,16 +1,38 @@
 import { BridgeComponent } from "@hotwired/hotwire-native-bridge"
+import { BridgeElement } from "@hotwired/hotwire-native-bridge"
 
 export default class extends BridgeComponent {
   static component = "overflow-menu"
+  static targets = [ "item" ]
 
   connect() {
     super.connect()
+    if (!this.hasItemTarget) return
     this.notifyBridgeOfConnect()
   }
 
+  disconnect() {
+    super.disconnect()
+    this.notifyBridgeOfDisconnect()
+  }
+
   notifyBridgeOfConnect() {
-    this.send("connect", this.bridgeElement.getButton(), () => {
-      this.element.click()
+    const items = this.itemTargets.map((target, index) => {
+      const element = new BridgeElement(target)
+      return { title: element.title, index }
     })
+
+    this.send("connect", { items }, message => {
+      this.activateItem(message)
+    })
+  }
+
+  notifyBridgeOfDisconnect() {
+    this.send("disconnect")
+  }
+
+  activateItem(message) {
+    const selectedIndex = message.data.selectedIndex
+    this.itemTargets[selectedIndex].click()
   }
 }
