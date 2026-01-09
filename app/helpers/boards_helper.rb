e30e7e2efd1d1d75f834bee0452ba7c409506609
Adrien Maston <adrien@37signals.com>
2026-01-09 11:57:16 +0100

    Don't make all buttons bigger on mobile

--- app/helpers/boards_helper.rb
+++ app/helpers/boards_helper.rb
@@ -4,7 +4,7 @@ module BoardsHelper
   end
 
   def link_to_edit_board(board)
-    link_to edit_board_path(board), class: "btn", data: { controller: "tooltip" } do
+    link_to edit_board_path(board), class: "btn btn--circle-mobile", data: { controller: "tooltip" } do
       icon_tag("settings") + tag.span("Settings for #{board.name}", class: "for-screen-reader")
     end
   end
