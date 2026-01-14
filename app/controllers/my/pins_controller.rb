73d7f2e2fe4d26ca186b5616ee65098b86c7c136
Denis Švara <denis.svara@gmail.com>
2026-01-14 17:43:16 +0100

    Add JSON support for GET `/my/pins` with no pagination and tests.

--- app/controllers/my/pins_controller.rb
+++ app/controllers/my/pins_controller.rb
@@ -1,6 +1,15 @@
 class My::PinsController < ApplicationController
   def index
-    @pins = Current.user.pins.includes(:card).ordered.limit(20)
+    @pins = pins_for_request
     fresh_when etag: [ @pins, @pins.collect(&:card) ]
   end
+
+  private
+    def pins_for_request
+      if request.format.json?
+        Current.user.pins.includes(:card).ordered
+      else
+        Current.user.pins.includes(:card).ordered.limit(20)
+      end
+    end
 end
