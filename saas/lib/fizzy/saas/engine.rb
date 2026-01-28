28f57bd962f00cafc1e1e124f5b1722faf303e02
Mike Dalessio <mike@37signals.com>
2026-01-28 11:59:46 -0500

    saas: Set lifetime of console1984 sessions to 60 days

--- saas/lib/fizzy/saas/engine.rb
+++ saas/lib/fizzy/saas/engine.rb
@@ -124,6 +124,7 @@ module Fizzy
         config.console1984.protected_environments = %i[ production beta staging ]
         config.console1984.ask_for_username_if_empty = true
         config.console1984.base_record_class = "::SaasRecord"
+        config.console1984.incinerate_after = 60.days
 
         config.audits1984.base_controller_class = "::SaasAdminController"
         config.audits1984.auditor_class = "::Identity"
