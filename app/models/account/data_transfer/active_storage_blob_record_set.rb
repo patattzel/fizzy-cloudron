9fcc44668373cb792dc835114d543d8d16463494
Stanko K.R. <stanko@stanko.io>
2026-02-02 15:22:16 +0100

    Explicitly use the default storage service

--- app/models/account/data_transfer/active_storage_blob_record_set.rb
+++ app/models/account/data_transfer/active_storage_blob_record_set.rb
@@ -6,4 +6,17 @@ class Account::DataTransfer::ActiveStorageBlobRecordSet < Account::DataTransfer:
       attributes: ActiveStorage::Blob.column_names - %w[service_name]
     )
   end
+
+  private
+    def import_batch(files)
+      batch_data = files.map do |file|
+        data = load(file)
+        data.slice(*attributes).merge(
+          "account_id" => account.id,
+          "service_name" => ActiveStorage::Blob.service.name
+        )
+      end
+
+      model.insert_all!(batch_data)
+    end
 end
