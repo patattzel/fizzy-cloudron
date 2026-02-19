4999ba27dd2aed5dbc37e2a8c09aaf02a0c07558
Adrien Maston <adrien@37signals.com>
2026-02-19 14:04:14 +0100

    Don't render empty state instead of hiding it

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -3,10 +3,10 @@ class SearchesController < ApplicationController
 
   def show
     @query = params[:q].blank? ? nil : params[:q]
-    if card = Current.user.accessible_cards.find_by_id(params[:q])
+    if card = Current.user.accessible_cards.find_by_id(@query)
       @card = card
     else
-      set_page_and_extract_portion_from Current.user.search(params[:q])
+      set_page_and_extract_portion_from Current.user.search(@query)
       @recent_search_queries = Current.user.search_queries.order(updated_at: :desc).limit(10)
     end
   end
