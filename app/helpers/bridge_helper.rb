a35c028535ad8e47e0316c944a386c7a4939cc9d
Rosa Gutierrez <rosa@37signals.com>
2026-02-20 12:58:20 +0100

    Revert commits accidentally pushed to main
    Revert "Add bridged share button to board and card permas"
    This reverts commit df622efd0d7b03f7791b1fbfe7e2e3b487b2ef5a.
    
    Revert "Move helper to bridge helper file"
    This reverts commit bef5f039334c9078deb6079b2464a3d2e700b9be.

--- app/helpers/bridge_helper.rb
+++ app/helpers/bridge_helper.rb
@@ -2,22 +2,4 @@ module BridgeHelper
   def bridge_icon(name)
     asset_url("#{name}.svg")
   end
-
-  def bridged_button_to_board(board)
-    link_to "Go to #{board.name}", board, hidden: true, data: {
-      bridge__buttons_target: "button",
-      bridge_icon_url: bridge_icon("board"),
-      bridge_title: "Go to #{board.name}"
-    }
-  end
-
-  def bridged_share_button(type = nil)
-    tag.button "Share", hidden: true, data: {
-      controller: "bridge--share",
-      action: "bridge--share#share",
-      bridge__overflow_menu_target: "item",
-      bridge_title: "Share",
-      bridge_share_type: type
-    }
-  end
 end
